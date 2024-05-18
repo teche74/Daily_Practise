b7ee497ffe45947c54600f61de90d3ac0aaee750
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-05-18 23:27:49 +0530

    Time: 0 ms (100%), Space: 14.6 MB (21.58%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
@@ -12,7 +12,6 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        if(root->left->val + root->right->val == root->val) return true;
-        return false;
+        return root->val == root->left->val + root->right->val;
     }
 };
