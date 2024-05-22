02f99cf1e55054ba1ca1c936a3bce38394343d57
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-05-22 21:27:43 +0530

    Time: 4 ms (53.35%), Space: 7.7 MB (67.36%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -3,36 +3,36 @@ public:
     bool isMatch(string s, string p) {
         int m = s.size() , n = p.size();
 
-        bool check[m+1][n+1];
+        bool dp[m+1][n+1] ;
 
-        memset(check,false,sizeof(check));
+        memset(dp,false,sizeof(dp));
 
-        check[0][0] = true;
+        dp[0][0] = true;
 
         for(int i = 1 ; i <= m ; i++){
-            check[i][0] = false;
+            dp[i][0] = false;
         }
 
-        for(int i = 1; i <= n ; i++){
+        for(int i =1; i <= n; i++){
             if(p[i-1] == '*'){
-                check[0][i] = check[0][i-2];
+                dp[0][i] =  dp[0][i -2];
             }
-        }
+        } 
 
-        for(int i =1 ;i <= m ;i++){
-            for(int j =1; j <= n ;j++){
-                if(s[i-1] == p[j-1] || p[j-1] == '.'){
-                    check[i][j] = check[i-1][j-1];
+        for(int i =1; i<= m;i++){
+            for(int j=1;j<=n;j++){
+                if(p[j-1] == s[i-1] || p[j-1] == '.'){
+                    dp[i][j] = dp[i-1][j-1];
                 }
                 else if(p[j-1] == '*'){
-                    check[i][j] = check[i][j-2] || (check[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'));
+                    dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'));
                 }
                 else{
-                    check[i][j] =  false;
+                    dp[i][j] = false;
                 }
             }
         }
 
-        return check[m][n];
+        return dp[m][n];
     }
 };
