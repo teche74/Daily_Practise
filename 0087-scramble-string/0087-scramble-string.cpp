d1ea0a178d114c102442cf230162dc74196ff40e
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-05-27 21:41:43 +0530

    Time: 152 ms (19.98%), Space: 38.4 MB (17.78%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    unordered_map<string,bool> map;
+    unordered_map<string,bool>map;
     bool isScramble(string s1, string s2) {
         if(s1 == s2) return true;
 
         if(s1.size() != s2.size()) return false;
 
-        int size = s1.size();
-
         string key = s1 + '.' + s2;
-
         if(map.count(key)) return map[key];
 
-        for(int i =1; i < size; i++)
-        {
-            if(isScramble(s1.substr(0, i) , s2.substr( size-i, i)) && isScramble(s1.substr(i, size- i) , s2.substr(0, size-i))){
-                map[key] = true;
-                return true;
-            }
+        int size = s1.size();
 
-            if(isScramble(s1.substr(0, i) , s2.substr(0, i)) && isScramble(s1.substr(i, size- i) , s2.substr(i, size-i))){
+        for(int i =1; i <size; i++){
+            if( isScramble( s1.substr(0,i) , s2.substr(size- i ,i) ) && isScramble( s1.substr(i,size- i) , s2.substr(0, size- i) ) || isScramble( s1.substr(0,i) , s2.substr(0,i) ) && isScramble( s1.substr(i,size- i) , s2.substr(i, size - i) )  ){
                 map[key] = true;
                 return true;
             }
         }
-
-        map[key] = false;
-        return map[key];
+        return map[key]  = false;
     }
 };
