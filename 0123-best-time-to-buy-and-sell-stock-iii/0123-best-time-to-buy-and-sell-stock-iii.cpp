dccba1660b166ff7f26403e7d958222278e073e5
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-05-27 22:44:44 +0530

    Time: 265 ms (48.8%), Space: 148.7 MB (48.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int> & arr, int index, int moves,vector<vector<int>> &dp){
-        if(index  ==  arr.size()){
-            return 0;
-        }
-
-        if(moves == 0){
+    int solve(int index, vector<int> & arr, int moves, vector<vector<int>> & dp){
+        if(index == arr.size()){
             return 0;
         }
 
 
-        if(dp[index][moves] != -1) return dp[index][moves]; 
+        if(!moves) {
+            return 0;
+        }
 
-        int take = 0;
+        if(dp[index][moves] != -1) return dp[index][moves];
 
-        if( !(moves & 1) ){
-            take = -arr[index] + solve(arr,index+1, moves-1,dp);
+        int take,nottake;
+        if( !(moves &1 ) ){
+            take = -arr[index] + solve(index+1,arr,moves-1,dp); // khreda 
         }
         else{
-            take  = arr[index] + solve(arr,index+1,moves-1,dp); 
+            take = arr[index] + solve(index+1,arr,moves-1,dp); // becha
         }
 
-        int not_take = solve(arr,index+1,moves,dp);
+        nottake=  solve(index+1,arr,moves,dp);
 
-        dp[index][moves] = max(take, not_take);
+        return dp[index][moves] = max(take,nottake);
 
-        return dp[index][moves];
     }
     int maxProfit(vector<int>& prices) {
         int size = prices.size();
         vector<vector<int>>dp(size, vector<int>(5, -1));
-        return solve(prices, 0, 4,dp);
+        return solve(0 , prices, 4, dp);
     }
 };
