ec5941633c6ff25c6c0485de5ddbc245dabd41b0
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-01 21:13:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.89%) - LeetHub

--- 1080-camelcase-matching/1080-camelcase-matching.cpp
+++ 1080-camelcase-matching/1080-camelcase-matching.cpp
@@ -1,39 +1,5 @@
-struct Node {
-    Node* links[128];
-    bool end = false;
-    int ind = -1;
-
-    void CreateRef(char ch, Node* node) { links[ch] = node; }
-
-    Node* GetRef(char ch) { return links[ch]; }
-
-    bool CheckRef(char ch) { return links[ch] != nullptr; }
-
-    bool IsEnd() { return end == true; }
-
-    void SetEnd(bool val) { end = val; }
-};
-
 class Solution {
-    Node* root = new Node();
-
 public:
-    void Init(vector<string>& strs) {
-        int i = 0;
-        for (string str : strs) {
-            Node* temp = root;
-            for (char ch : str) {
-                if (!temp->CheckRef(ch)) {
-                    temp->CreateRef(ch, new Node());
-                }
-                temp = temp->GetRef(ch);
-            }
-            temp->SetEnd(true);
-            temp->ind = i;
-            i++;
-        }
-    }
-
     bool MatchPattern(string& query, string& pattern) {
         int i = 0, j = 0;
         while (i < query.size() && j < pattern.size()) {
