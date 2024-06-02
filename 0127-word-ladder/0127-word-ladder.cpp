25d9a2a047ba9568aaa436acd80c0913b642c3de
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-02 15:01:35 +0530

    Time: 910 ms (18.06%), Space: 33.5 MB (22.34%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        unordered_set<string>st(wordList.begin() , wordList.end());
+        unordered_set<string>st(wordList.begin(),wordList.end());
 
         queue<string>q;
-
         q.push(beginWord);
-        int len = 1;
+        int level =1;
 
         while(q.size()){
             int size = q.size();
 
-            for(int i =0; i <size; i++){
-                string front = q.front();
+            for(int i =0; i <size ; i++){
+                string ptr = q.front();
                 q.pop();
-
-                if(front == endWord){
-                    return len;
+                if(ptr == endWord){
+                    return level;
                 }
-                st.erase(front);
+                st.erase(ptr);
 
-                for(int i =0; i < front.size() ;i++){
-                    char ch = front[i];
-                    for(int k =0; k < 26 ;k ++){
-                        front[i] = 'a' + k;
+                for(int j =0; j < ptr.size() ; j++){
+                    char ch = ptr[j];
 
-                        if(st.find(front) != st.end()){
-                            q.push(front);
+                    for(char k = 'a' ; k <= 'z' ; k++){
+                        ptr[j] = k;
+
+                        if(st.find(ptr) != st.end()){
+                            q.push(ptr);
                         }
-                    }
-                    front[i] = ch;
+                    } 
+
+                    ptr[j] = ch;
                 }
             }
-                len++;
+            level++;
         }
         return 0;
     }
