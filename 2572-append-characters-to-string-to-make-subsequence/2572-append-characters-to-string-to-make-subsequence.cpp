8b9b7105adf0964f41d20da9159d9e3694c2cdc0
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-03 09:44:23 +0530

    Time: 23 ms (20.38%), Space: 12 MB (11.4%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    void solve(int i, int j, const string &s, const string &t, int &count) {
-        if (j == t.size()) {
-            count = 0;
-            return;
-        }
-        if (i == s.size()) {
-            count = t.size() - j;
-            return;
-        }
-
-        if (s[i] == t[j]) {
-            solve(i + 1, j + 1, s, t, count);
-        } else {
-            int tempCount = INT_MAX;
-            solve(i + 1, j, s, t, tempCount);
-            count = min(count, tempCount);
-        }
-    }
-
     int appendCharacters(string s, string t) {
-        int count = INT_MAX;
-        solve(0, 0, s, t, count);
-        return count;
+        int m = s.size() , n = t.size();
+
+        int j =0;
+
+        for(int i =0;i < m; i++){
+            if(s[i] == t[j]){
+                j++;
+            }
+        }
+
+        return n-j;
     }
 };
