2df52d1b5ab883fd49d4195a4c7d41ce5656e629
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-15 10:29:51 +0530

    Time: 3 ms (90.73%), Space: 14.7 MB (99.94%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int size = nums.size();
-        vector<int>res(size * 2, 0);
 
-        for(int i =0; i <size ; i++){
-            int ind1 = i , ind2 = ind1+nums.size();
-            cout<<ind1<<" "<<ind2<<endl;
-            res[ind1] = nums[i];
-            res[ind2] = nums[i];
+        vector<int>res(size*2,0);
+
+        for(int i = 0 ; i < size ; i++){
+            res[i] = nums[i];
+            res[i+size] = nums[i];
         }
 
         return res;
