64034e3dc2be6d271123e8cd5644e081cd3d17f3
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-22 10:11:53 +0530

    Time: 4 ms (98.08%), Space: 8.8 MB (72.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -26,7 +26,59 @@ public:
 
         return s.substr(ind, len);
     }
+
+    string manchars(const string & s){
+        string temp = "$";
+
+        for(char ch : s){
+            temp.push_back('|');
+            temp.push_back(ch);
+        }
+
+        temp.push_back('|');
+        temp.push_back('#');
+        
+
+        int lps[temp.size()];
+
+        memset(lps, 0, sizeof(lps));
+
+        int right = 0 , centre = 0;
+
+        for(int i =1; i < temp.size()-1 ; i++){
+            int mirror = 2 * centre  - i;
+
+            if (i < right) {
+                lps[i] = min(right - i, lps[mirror]);
+            }
+
+
+            while( temp[i + lps[i] + 1] == temp[i - lps[i] - 1]){
+                lps[i]++;
+            }
+
+            if( i + lps[i] > right ){
+                right = i+ lps[i];
+                centre = i;
+            }
+        }
+
+        int max_len = 0 , max_centre = 0;
+
+        for(int i =1; i < temp.size()-1 ; i++){
+            if(lps[i] > max_len){
+                max_len = lps[i];
+                max_centre = i;
+            }
+        }
+
+        int ind = (max_centre - max_len)/2;
+
+        return s.substr(ind,max_len);
+
+    }
     string longestPalindrome(string s) {
-        return partition(s);
+        // return partition(s);
+        return manchars(s);
     }
 };
