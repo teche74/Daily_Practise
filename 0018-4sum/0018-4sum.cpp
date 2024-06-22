0ec7f9a1905d0e10ff5c00c773d50a971c4f802c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-22 11:09:30 +0530

    Time: 29 ms (49.45%), Space: 16 MB (34.25%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+    vector<vector<int>> fourSum(vector<int>& arr, int val) {
         int size = arr.size();
-
-        sort(arr.begin(), arr.end());
-
+        sort(arr.begin(),arr.end());
         vector<vector<int>>res;
+        for(int i =0; i <size;i++){
+            if(i >0 && arr[i] == arr[i-1]) continue;
 
-        for(int i =0 ;i <size; i++){
-            if(i > 0 && arr[i] == arr[i-1]) continue;
+            for(int j = i+1;j<size ;j++){
+                if( j> i+1 && arr[j] == arr[j-1]) continue;
 
-            for(int l = i+1 ; l < size ; l++){
-                if(l  > i+1 && arr[l] == arr[l-1]) continue;
-                int j = l+1, k =size-1;
+                int k = j+1;
+                int l = size-1;
 
-                while(j< k){
-                    long long sum = arr[i];
+                while(k<l){
+                    long  long sum = arr[i];
                     sum+=arr[j];
                     sum+=arr[k];
-                    sum+=arr[l];
+                    sum+= arr[l];
 
-                    if(sum == target){
-                        res.push_back({arr[i],arr[j],arr[k], arr[l]});
-                        j++;
-                        k--;
-                        while(j < k && arr[j] == arr[j-1]) j++;
-                        while(j <k && arr[k] == arr[k+1]) k--;
+                    if(sum  == val){
+                        vector<int>temp = {arr[i],arr[j],arr[k],arr[l]};
+                        res.push_back(temp);
+
+                        k++;
+                        l--;
+
+                        while(k<l && arr[k] == arr[k-1]) k++;
+                        while(k<l && arr[l] == arr[l+1]) l--;
                     }
-                    else if(sum > target){
-                        k--;
+
+                    else if(sum > val){
+                        l--;
                     }
                     else{
-                        j++;
+                        k++;
                     }
                 }
             }
