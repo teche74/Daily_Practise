fcf3e43b1e5b6184451afbb7f0cf91be91e2525f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-23 19:48:51 +0530

    Time: 0 ms (100%), Space: 16 MB (86.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int tar) {
-        auto get = [&arr, &tar](bool val){
-            int low =0 , high = arr.size()-1;
-            int ans= -1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int  low =  0 , high = 0, size = nums.size();
 
-            while(low <= high){
-                int mid = low + ( high - low)/2;
+        for(int i = size ;i >=1 ; i/=2){
+            while(low + i < size  && nums[low+i] <target) low+=i;
+            while(high + i < size && nums[high+i] <= target) high+=i;
+        }
 
-                if(arr[mid] == tar){
-                    ans = mid;
+        if(low < size && nums[low] == target) return {low, high};
 
-                    if(val){
-                        low = mid+1;
-                    }
-                    else{
-                        high = mid-1;
-                    }
-                }
-                else if(arr[mid] > tar){
-                    high  = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            return ans;
-        };
+        if(low + 1 >= size ||  nums[low + 1] != target) return {-1,-1};
+        if(nums[low+1] == target && (low +2 < size && nums[low+2] != target) || low+2 >=size) return {low+1, low+1};
 
+        return {low+1, high};
 
-        return {get(false),get(true)};
     }
 };
