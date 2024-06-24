1b40aca16c5883acd3251bfa08b2a76fca193da2
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-24 19:18:06 +0530

    Time: 39 ms (20.96%), Space: 29.4 MB (21.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
+    unordered_map<string, vector<vector<int>>> memo;
 
-    void solve(int index, vector<int> & arr, int val, vector<int>temp, vector<vector<int>> &res){
-        if(index >= arr.size()){
-            if(val == 0){
-                res.emplace_back(temp);
+    vector<vector<int>> solve(int index, vector<int>& arr, int val) {
+        string key = to_string(index) + "-" + to_string(val);
+        
+        if (memo.find(key) != memo.end()) {
+            return memo[key];
+        }
+
+        if (val == 0) {
+            return {{}};
+        }
+
+        if (index >= arr.size() || val < 0) {
+            return {};
+        }
+
+        vector<vector<int>> res;
+
+        if (val - arr[index] >= 0) {
+            vector<vector<int>> include = solve(index, arr, val - arr[index]);
+            for (auto& combination : include) {
+                combination.emplace_back(arr[index]);
+                res.emplace_back(combination);
             }
-            return;
         }
 
-        if(val - arr[index] >= 0){
-            temp.emplace_back(arr[index]);
-            solve(index,arr,val-arr[index] , temp,res);
-            temp.pop_back();
-        }
-        solve(index+1,arr,val,temp,res);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
+        vector<vector<int>> exclude = solve(index + 1, arr, val);
+        res.insert(res.end(), exclude.begin(), exclude.end());
 
-        vector<int>temp;
-
-        solve(0, candidates, target,temp,res);
-
+        memo[key] = res;
         return res;
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        memo.clear();
+        return solve(0, candidates, target);
     }
 };
