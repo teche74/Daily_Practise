3f7ab547f91ec3de87e223ed56612c8409c8a847
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-24 20:38:52 +0530

    Time: 0 ms (100%), Space: 8 MB (44.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        unordered_map<int, double> memo; 
-
-        return powHelper(x, n, memo);
-    }
-
-private:
-    double powHelper(double x, int n, unordered_map<int, double>& memo) {
-        if (n == 0)
-            return 1;
-        if (n == 1)
-            return x;
-        if (n == -1)
-            return 1 / x;
-
-        if (memo.find(n) != memo.end())
-            return memo[n];
-
-        double result = powHelper(x, n / 2, memo);
-        result *= result;
-
-        if (n % 2 != 0) {
-            if (n > 0)
-                result *= x;
-            else
-                result *= 1 / x;
+    double solve(int n, double a){
+        double ans=1;
+        for(;n; n/=2){
+                if (n & 1) {
+                     ans *= a;
+                }
+            a *= a;
         }
-
-        memo[n] = result;
-        return result;
+        return  ans;
+    }
+    double myPow(double x, int n) {
+        return n >= 0  ? solve(n,x) : 1/solve(n,x);
     }
 };
