4fc80d2a59ef2b527e1683f92b53581804d88104
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-25 07:46:55 +0530

    Time: 11 ms (18.61%), Space: 12.7 MB (36.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -19,8 +19,27 @@ public:
         return memo[row][col] = arr[row][col] + min(down,right);
     }
     int minPathSum(vector<vector<int>>& grid) {
+        // vector<vector<int>>memo(rows, vector<int>(cols,-1));
+        // return solve(0,0,grid,memo);
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<int>>memo(rows, vector<int>(cols,-1));
-        return solve(0,0,grid,memo);
+
+        vector<vector<int>>dp(rows, vector<int>(cols,0));
+        
+        dp[0][0] = grid[0][0];
+
+        for(int i =1; i < rows; i++){
+            dp[i][0] += dp[i-1][0] + grid[i][0]; 
+        }
+
+        for(int j=1;j<cols;j++){
+            dp[0][j] += dp[0][j-1] + grid[0][j];
+        }
+
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<cols;j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[rows-1][cols-1];
     }
 };
