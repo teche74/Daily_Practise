dbd2a7b7cfc1ad2481668df4979c05898b6a4b3c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-28 22:26:46 +0530

    Time: 7 ms (69.83%), Space: 26.5 MB (90.97%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
@@ -1,65 +1,15 @@
-struct Node {
-    Node* bits[2] = {nullptr, nullptr};
-
-    void createBit(int bit, Node* node) {
-        bits[bit] = node;
-    }
-
-    bool checkBit(int bit) {
-        return bits[bit] != nullptr;
-    }
-
-    Node* getBit(int bit) {
-        return bits[bit];
-    }
-};
-
 class Solution {
-    Node* root = new Node();
-
-    void insert(int num) {
-        Node* node = root;
-        for (int i = 31; i >= 0; --i) {
-            int bit = (num >> i) & 1;
-            if (!node->checkBit(bit)) {
-                node->createBit(bit, new Node());
-            }
-            node = node->getBit(bit);
-        }
-    }
-
-    int FindXor(int num) {
-        Node* node = root;
-        int maxXOR = 0;
-        for (int i = 31; i >= 0; --i) {
-            int bit = (num >> i) & 1;
-            if (node->checkBit(1 - bit)) {
-                maxXOR |= (1 << i);
-                node = node->getBit(1 - bit);
-            } else {
-                node = node->getBit(bit);
-            }
-        }
-        return maxXOR;
-    }
-
 public:
-    int maximumStrongPairXor(std::vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    int maximumStrongPairXor(vector<int>& nums) {
+        int size = nums.size(), res = 0;
 
-        int res = 0;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = i; j < nums.size(); ++j) {
-                int x = nums[i];
-                int y = nums[j];
-                if (abs(x - y) <= min(x, y)) {
-                    insert(y);
-                    res = max(res, FindXor(x));
+        for(int i =0 ; i < size ; i++){
+            for(int j = i+1; j <size; j++){
+                if( abs(nums[i] - nums[j])  <= min(nums[i], nums[j])){
+                    res = max(res, nums[i] ^ nums[j]);
                 }
             }
-            root = new Node();  
         }
-
         return res;
     }
 };
