bd5ab3dae60fb94adab169d0aaf019e6ec9e8bc8
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-28 20:19:27 +0530

    Time: 14 ms (26.1%), Space: 16.4 MB (88.23%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -11,13 +11,36 @@
  */
 class Solution {
 public:
+    map<pair<int,int>,vector<TreeNode*>>map;
     vector<TreeNode*> solve(int low, int high){
+        // vector<TreeNode*>res;
+
+        // if(low > high) {
+        //     res.emplace_back(nullptr);
+        //     return res;
+        // } 
+
+        // for(int i= low ; i <= high ; i++){
+        //    vector<TreeNode *>left = solve(low,i-1);
+        //    vector<TreeNode*> right = solve(i+1,high);
+
+        //    for(auto t : left){
+        //         for(auto r : right){
+        //              TreeNode * root = new TreeNode(i,t,r);
+        //              res.push_back(root);
+        //         }
+        //    }
+        // }
+        // return res;
+
         vector<TreeNode*>res;
 
         if(low > high) {
             res.emplace_back(nullptr);
             return res;
         } 
+
+        if(map.find({low,high}) != map.end()) return map[{low,high}]; 
 
         for(int i= low ; i <= high ; i++){
            vector<TreeNode *>left = solve(low,i-1);
@@ -30,7 +53,7 @@ public:
                 }
            }
         }
-        return res;
+        return map[{low, high}] = res;
     }
     vector<TreeNode*> generateTrees(int n) {
         int low{1}, high{n};
