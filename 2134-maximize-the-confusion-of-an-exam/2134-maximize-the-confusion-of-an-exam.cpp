f2843164ae4560cec3043c6ff3da478ea537af44
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-29 10:10:06 +0530

    Time: 20 ms (88.16%), Space: 11.2 MB (94.84%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string a, int k) {
-        int size = a.size();
+    int helper( const string & s , int k , char ch){
+        int low =0  , high = 0 , size = s.size() , res = 0 ;
+        int opers = 0;
 
-        int low = 0, high = 0;
 
-        int t = 0, f = 0;
+        while( high < size){
+            opers += (s[high] != ch);
 
-        int res = 0;
+            while(opers > k){
+                opers -= (s[low] != ch);
+                low++;
+            }   
 
-        while (high < size) {
-            if (a[high] == 'T') t++;
-            else f++;
-
-            int maxi = max(t, f);
-            int val = t + f;
-
-            if (val - maxi <= k) {
-                res = max(res, val);
-            }
-            else {
-                while (val - maxi > k) {
-                    if (a[low] == 'T') t--;
-                    else f--;
-
-                    low++;
-                    val = t + f;
-                    maxi = max(t, f);
-                }
-
-                res = max(res, val); 
-            }
-
+            res = max(res,high - low+1 );
             high++;
         }
-
         return res;
+    }
+    int maxConsecutiveAnswers(string arr, int k) {
+        return max(helper(arr,k,'T'), helper(arr,k,'F')); 
     }
 };
