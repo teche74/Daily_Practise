89c5fe6e8a90793872a2c18678ad3d7f2c38955b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-06-29 10:38:14 +0530

    Time: 65 ms (100%), Space: 62.9 MB (97.91%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-         vector<vector<int>> adj(n);
-        vector<int> inDegree(n, 0);
-        vector<set<int>> ancestors(n);
+        vector<vector<int>> adj(n), ans(n);
+
+        for (auto& t: edges) {
+            adj[t[0]].push_back(t[1]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dfs(adj,ans,i,i);
+        }
         
-        for (const auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            inDegree[edge[1]]++;
-        }
-
-        queue<int> q;
-        for (int i = 0; i < n; ++i) {
-            if (inDegree[i] == 0) {
-                q.push(i);
+        return ans;
+    }
+    void dfs(vector<vector<int>>& adj, vector<vector<int>>& ans, int& parent, int& child) {
+        for (auto& ch: adj[child]) {
+            if (ans[ch].size() == 0 || ans[ch].back() != parent) {
+                ans[ch].push_back(parent);
+                dfs(adj,ans,parent,ch);
             }
+            
         }
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            for (int neighbor : adj[node]) {
-                ancestors[neighbor].insert(node);
-                for (int anc : ancestors[node]) {
-                    ancestors[neighbor].insert(anc);
-                }
-                if (--inDegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
-        }
-
-        vector<vector<int>> result(n);
-        for (int i = 0; i < n; ++i) {
-            result[i] = vector<int>(ancestors[i].begin(), ancestors[i].end());
-        }
-
-        return result;
     }
 };
