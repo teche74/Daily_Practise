a45d467e284d2f4e3f4b03d857954fee737e4b28
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-02 10:30:10 +0530

    Time: 47 ms (56.89%), Space: 26.6 MB (28.06%) - LeetHub

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minFlips(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int size = s.size();
 
         string pattern1= "" ,  pattern2 = "";
@@ -23,10 +25,10 @@ public:
 
 
         for(int i = size ; i  < s.size() ;i++){
-            if(s[i- size] != pattern1[i- size]) flip1--;
-            if(s[i-size] != pattern2[i-size]) flip2--;
+            if(s[i- size] != pattern1[i- size]) flip1--; // agr last or first element different he to simple shift krna he operation perform ki jrurt nhi isliye flip minus kiya .
+            if(s[i-size] != pattern2[i-size]) flip2--; // same upar vala logic for pattern2.
 
-            if(s[i] != pattern1[i] ) flip1++;
+            if(s[i] != pattern1[i] ) flip1++; // ab jab window ki batt hori he to dhyan rkhna he ki jo operation perform hua vo window ke andr vale elements ke liye hi ho jesi hi bhar jata to flips increase kr do . 
             if(s[i] != pattern2[i] ) flip2++;
 
 
