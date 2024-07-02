2fff111869c81f905fb1abba8d617c5f48851d4a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-03 00:03:21 +0530

    Time: 5 ms (87.45%), Space: 9 MB (99.59%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void removeInvalidParenthesesHelper(string &s, int start, int lastRemove, char openParen, char closeParen, vector<string> &res) {
+    void helper(string &s, int start, int last_remove, char open, char close, vector<string> &res) {
         for (int stack = 0, i = start; i < s.length(); ++i) {
-            if (s[i] == openParen) stack++;
-            if (s[i] == closeParen) stack--;
+            if (s[i] == open) stack++;
+            if (s[i] == close) stack--;
             if (stack >= 0) continue;
 
-            for (int j = lastRemove; j <= i; ++j) {
-                if (s[j] == closeParen && (j == lastRemove || s[j - 1] != closeParen)) {
+            for (int j = last_remove; j <= i; ++j) {
+                if (s[j] == close && (j == last_remove || s[j - 1] != close)) {
                     string curr = s.substr(0, j) + s.substr(j + 1);
-                    removeInvalidParenthesesHelper(curr, i, j, openParen, closeParen, res);
+                    helper(curr, i, j, open, close, res);
                 }
             }
             return;
         }
         reverse(s.begin(), s.end());
-        if (openParen == '(')
-            removeInvalidParenthesesHelper(s, 0, 0, ')', '(', res);
+        if (open == '(')
+            helper(s, 0, 0, ')', '(', res);
         else
             res.push_back(s);
     }
 
     vector<string> removeInvalidParentheses(string s) {
         vector<string> res;
-        removeInvalidParenthesesHelper(s, 0, 0, '(', ')', res);
+        helper(s, 0, 0, '(', ')', res);
         return res;
     }
 };
