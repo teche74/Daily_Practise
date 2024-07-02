0fddf928d413f1ce4b795344e08d7544d9ac9f8e
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-02 10:58:58 +0530

    Time: 44 ms (98.69%), Space: 82.5 MB (52.58%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
@@ -1,3 +1,10 @@
+    auto init = []()
+    { 
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        return 'c';
+    }();
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
