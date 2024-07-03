7322d4cef90b7f55ccf757489a17e5d0473c43e9
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-03 11:03:59 +0530

    Time: 0 ms (100%), Space: 7.7 MB (33.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
         
-        auto isopen = [](char ch)->bool{
-            return ch == '(' || ch == '[' || ch == '{';
+        function<bool(char , char)> check = [](char val1 , char val2){
+            return (val1 == '('  && val2 == ')' || val1 == '[' && val2 ==']' || val1 == '{' && val2 =='}');  
         };
 
-        auto match = [](char ch , char ch2)->bool{
-            if(ch == '(' && ch2 == ')') return true;
-            if(ch == '[' && ch2 == ']') return true;
-            if(ch == '{' && ch2 == '}') return true;
-            return false; 
-        }; 
 
-         for (char ch : s) {
-            if (isopen(ch)) {
-                st.push(ch);
-            } else {
-                if (st.empty() || !match(st.top(), ch)) {
-                    return false;
-                }
-                st.pop();
+        stack<char>st;
+
+        for(auto t : s){
+            if(t =='(' || t =='[' || t =='{'){
+                st.push(t);
+            }
+            else{
+                if(st.empty() || !check(st.top(),t)) return false;
+                else st.pop();
             }
         }
 
