ea00c2539ad55f15787d61864532f82d9f37d627
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-03 11:37:19 +0530

    Time: 4 ms (43.11%), Space: 13.3 MB (50.82%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string s) {
+        int size = s.size();
 
-       function<int(int,int,char)> calc = [](int num1, int num2 , char oper){
-            if(oper == '+'){
-                return num1 + num2;
-            }
-            else if(oper == '-'){
-                return num1 - num2;
-            }
-            else if(oper == '*'){
-                return num1 * num2;
-            }
+        function<int(int,int,char)> calc = [&](int val1, int val2, char oper){
+            if(oper == '+') return val1 + val2;
+            if(oper == '-') return val1 - val2;
+            if(oper == '*') return val1 * val2;
             return 0;
-       };
+        };
 
-       vector<int>res;
-       bool flag = true;
-       int size = s.size();
+        vector<int>res;
+        bool flag =true;
 
-       for(int i =0; i <size; i++){
+        for(int i = 0 ;i < size ; i++){
             if(!isdigit(s[i])){
-                flag  =false;
+                flag = false;
                 vector<int>left = diffWaysToCompute(s.substr(0,i));
                 vector<int>right = diffWaysToCompute(s.substr(i+1));
 
-                for(int c : left){
-                    for(int t : right){
-                        res.emplace_back(calc(c,t,s[i]));
+                for(auto t : left){
+                    for(auto r : right){
+                        res.emplace_back(calc(t,r,s[i]));
                     }
                 }
             }
         }
 
-        if(flag) res.push_back(stoi(s));
-        
+        if(flag) res.emplace_back(stoi(s));
+
         return res;
     }
 };
