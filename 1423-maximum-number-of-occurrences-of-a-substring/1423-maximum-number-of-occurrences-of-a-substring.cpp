b4c2e21170f86ba479dedc23ed31e8e1cb684307
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-03 21:15:21 +0530

    Time: 47 ms (85.63%), Space: 16.7 MB (91.84%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int solve(string s, int maxLetters, int maxSize){
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         int low = 0 , high = 0 , size = s.size() , res= 0;
         int map[26];
         int count_uni = 0;
-        unordered_map<string,int>counter;
 
+        unordered_map<string,int>counter;
         memset(map,0,sizeof(map));
 
         while(high < size){
             map[s[high] - 'a']++;
             count_uni += (map[s[high] - 'a'] == 1);
 
-            while(high - low + 1 > maxSize || count_uni > maxLetters){
+            if(count_uni <= maxLetters && high - low +1 >= minSize){
+                string t = s.substr(low, high - low+1);
+                counter[t]++;
+                res = max(res,counter[t]);
+            }
+
+            while(high - low + 1 >= minSize ){
                 map[s[low] - 'a']--;
                 count_uni -= (map[s[low] - 'a'] == 0);
                 low++;
             }
-
-            int len  = high - low +1;
-            if(len == maxSize){
-                string str = s.substr(low, len);
-                counter[str]++;
-                res = max(res,counter[str]);
-            }
-
             high++;
-        }
-
-        return res;
-    }
-    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
-        int res = 0 ;
-
-        for(int i =minSize ;i <=maxSize ; i++){
-            res = max(res, solve(s,maxLetters,i));
         }
 
         return res;
