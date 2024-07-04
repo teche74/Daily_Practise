f29120303e1e1f1d9c4717c4996a011a560fcb68
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-04 12:08:31 +0530

    Time: 2 ms (49.73%), Space: 6.9 MB (99.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -14,17 +14,35 @@
 
 // using memoization
 
+// class Solution {
+// public:
+//     unordered_map<int,int>dp;
+//     int climbStairs(int n) {
+//         if(n <= 0) return 1;
+
+//         if(dp.count(n)) return dp[n];
+
+//         int take_one_step = n > 0  ?climbStairs(n-1) : 0;
+//         int take_two_steps = n > 1 ? climbStairs(n-2) : 0;
+
+//         return dp[n] = take_one_step + take_two_steps; 
+//     }
+// };
+
+// using tabulation
+
 class Solution {
 public:
-    unordered_map<int,int>dp;
     int climbStairs(int n) {
-        if(n <= 0) return 1;
+        int dp[n+1];
+        memset(dp, 0 ,sizeof(dp));
 
-        if(dp.count(n)) return dp[n];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        int take_one_step = n > 0  ?climbStairs(n-1) : 0;
-        int take_two_steps = n > 1 ? climbStairs(n-2) : 0;
-
-        return dp[n] = take_one_step + take_two_steps; 
+        for(int i =2;i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        } 
+        return dp[n];
     }
 };
