5d863d1ea6559b1bbc0b73cf433557333595ec74
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-04 08:28:40 +0530

    Time: 3 ms (79.15%), Space: 8.8 MB (64.82%) - LeetHub

--- 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.cpp
+++ 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     int maxRepOpt1(string text) {
-        // Step 1: Run-length encoding
-        vector<pair<char, int>> rle;
+        vector<vector<int>> indices(26);
+
         int size = text.size();
-        int prev_ind = 0, count = 1; 
-        
-        for(int i = 1; i < size; i++) {
-            if(text[i] == text[prev_ind]) {
-                count++;
-            } else {
-                rle.push_back({text[prev_ind], count});
-                prev_ind = i;
-                count = 1;
-            }
+
+        for (int i = 0; i < size; ++i) {
+            indices[text[i] - 'a'].push_back(i);
         }
-        rle.push_back({text[prev_ind], count});
-        
-        unordered_map<char, int> freq;
-        for (auto& p : rle) {
-            freq[p.first] += p.second;
-        }
-        
-        int maxLength = 0;
-        
-        for (int i = 0; i < rle.size(); ++i) {
-            int currentLength = rle[i].second;
-            
-            if (freq[rle[i].first] > currentLength) {
-                currentLength++;
-            }
-            
-            if (i > 0 && i < rle.size() - 1 && rle[i - 1].first == rle[i + 1].first && rle[i].second == 1) {
-                int combinedLength = rle[i - 1].second + rle[i + 1].second;
-                if (freq[rle[i - 1].first] > combinedLength) {
-                    combinedLength++;
+
+        int res = 1;
+
+        for (int k = 0; k < 26; ++k) {
+            if (indices[k].empty()) continue;
+
+            int cnt = 1, extras = 0, max_count = 0;
+
+            for (int i = 1; i < indices[k].size(); ++i) {
+                if (indices[k][i] == indices[k][i - 1] + 1) {
+                    cnt++;
+                } else {
+                    extras = (indices[k][i] == indices[k][i - 1] + 2) ? cnt : 0;
+                    cnt = 1;
                 }
-                maxLength = max(maxLength, combinedLength);
+                max_count = max(max_count, cnt + extras);
             }
-            
-            maxLength = max(maxLength, currentLength);
+            res = max(res, max_count + ((indices[k].size() > max_count) ? 1 : 0));
         }
-        
-        return maxLength;
+
+        return res;
     }
 };
