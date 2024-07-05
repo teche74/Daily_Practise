f71358f7e267f5ad2d698237bb7ff6252d673db2
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-05 08:26:57 +0530

    Time: 6 ms (35.17%), Space: 16.1 MB (84.52%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -22,25 +22,44 @@
 
 // memo
 
+// class Solution {
+// public:
+//     vector<int> memo;
+//     int solve(int index, vector<int> & cost){
+//         if (index >= cost.size()) {
+//             return 0;
+//         }
+
+//         if(memo[index] != -1) return memo[index];
+        
+//         int takeOneStep = cost[index] + solve(index + 1, cost);
+//         int takeTwoSteps = cost[index] + solve(index + 2, cost);
+        
+//         return memo[index] = min(takeOneStep, takeTwoSteps);
+//     }
+
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n = cost.size();
+//         memo.assign(n, -1);
+//         return min(solve( 0, cost) , solve(1,cost));
+//     }
+// };
+
+
+
 class Solution {
 public:
-    vector<int> memo;
-    int solve(int index, vector<int> & cost){
-        if (index >= cost.size()) {
-            return 0;
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+
+        int prev = cost[0] , prev2 = cost[1];
+
+        for(int i =2; i < size; i++){
+            int curr = cost[i] + min(prev,prev2);
+            prev = prev2;
+            prev2 =curr;
         }
 
-        if(memo[index] != -1) return memo[index];
-        
-        int takeOneStep = cost[index] + solve(index + 1, cost);
-        int takeTwoSteps = cost[index] + solve(index + 2, cost);
-        
-        return memo[index] = min(takeOneStep, takeTwoSteps);
-    }
-
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        memo.assign(n, -1);
-        return min(solve( 0, cost) , solve(1,cost));
+        return min(prev,prev2);
     }
 };
