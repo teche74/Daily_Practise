432cc0f20248a1ea743596918da86fbcb5899d75
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-05 09:29:40 +0530

    Time: 0 ms (100%), Space: 7.1 MB (82.14%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int>dp(n+1, 0);
+        vector<int>dp(n+1,0);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        dp[0] =1; // 0 nodes me 1 bst posisible he;
 
-        for(int i = 2; i <= n; i++){
-            for(int j = 1 ;j <= i; j++){
-                dp[i]  += dp[j-1] * dp[i-j];
+        for(int nodes = 1 ; nodes <= n; nodes++){
+            
+            for(int left = 0; left < nodes ;left++){ // bst me left nodes ki possiblities
+
+                int right = nodes - left - 1; // total me se left ghtaya right milega 
+
+                dp[nodes] += (dp[left] * dp[right]) ; // left right milakr sare combinations.
+
             }
         }
 
