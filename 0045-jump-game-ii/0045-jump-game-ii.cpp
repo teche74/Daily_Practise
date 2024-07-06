8b71b46fdf9511e6da9a76b47cae09762b7ee535
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-06 08:47:10 +0530

    Time: 8 ms (81.68%), Space: 19 MB (62.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int size = nums.size();
+
         if(size <= 1) return 0;
-        int curr = 0 ,far = 0, jumps = 0;
 
+        int curr = 0, far = 0 , jumps = 0;
 
-        for(int i =0 ; i <size; i++){
-            far = max(nums[i]+i, far);
+        for(int i = 0 ;i  <size; i++){
+            far  = max(far , nums[i] + i);
 
             if(i == curr){
-                curr = far;
+                curr =far;
                 jumps++;
 
-                if(curr >= size-1) return jumps;
+                if(curr == size-1){
+                    return jumps;
+                }
             }
-        }
-
+        } 
         return jumps;
     }
 };
