1646ed5c8a5f0cedadf3bd46d7763f752f48357f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-06 08:49:25 +0530

    Time: 51 ms (31.92%), Space: 50.7 MB (81.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int size = nums.size();
+
         int curr = 0 , far = 0 ;
 
-        for(int i =0; i < nums.size() ; i++){
-            far = max(far,nums[i] + i);
+        for(int i =0 ;i <size; i++){
+            far = max(far , nums[i]  +i);
 
             if(curr == i){
                 curr = far;
 
-                if(curr >= nums.size()-1) return true;
+                if(curr >= size-1) return true;
             }
-        }
+        } 
+
         return false;
     }
 };
