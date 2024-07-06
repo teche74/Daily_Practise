4769f77d549a9c08d20577caa730cecb59ae1215
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-06 08:56:58 +0530

    Time: 2 ms (42.56%), Space: 7.2 MB (86.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        int dp[m][n];
+        memset(dp, 0, sizeof(dp));
 
-        for(int i =0; i < m; i++){
-            for(int j=0; j < n; j++){
-                if(i == 0 || j== 0){
-                    dp[i][j] = 1;
-                }
-                else
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+        // pura right jane ka sirf 1 i tareeka he (from 0 position);
+
+        for(int i = 0; i < m ;i++ ){
+            dp[i][0] =1;
+        }   
+
+        //  pura down jane ka sirf ek tareeka (from 0 position)
+
+        for(int i=0; i < n ; i++){
+            dp[0][i] = 1;
         }
 
+        for(int i=1; i<m;i++){
+            for(int j =1; j<n;j++){
+                // ab kisi bhi raste ke liye check krna he to simple uske right jane ke kitne tareeke  + down jane ke kitne tarreeke .
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
         return dp[m-1][n-1];
     }
 };
