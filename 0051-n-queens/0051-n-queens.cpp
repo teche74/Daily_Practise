29b3297d43890e005b0bb940e63f478c3567cf32
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-07 12:17:33 +0530

    Time: 56 ms (5.45%), Space: 53.9 MB (5.22%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    bool valid(vector<string>temp , int row, int col, int size){
-        for (int i = 0; i < row; i++) {
+    bool check(vector<string>temp,int row, int col){
+        for (int i = 0; i < row; ++i) {
             if (temp[i][col] == 'Q') return false;
         }
 
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+        for (int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
             if (temp[i][j] == 'Q') return false;
         }
 
-        for (int i = row - 1, j = col + 1; i >= 0 && j < size; i--, j++) {
+
+        for (int i = row, j = col; i >= 0 && j < temp.size(); --i, ++j) {
             if (temp[i][j] == 'Q') return false;
         }
 
         return true;
     }
-    void solve(int row, int len,  vector<vector<string>> & res, vector<string>temp){
-        if(row == len){
+    void solve(int row, int size, vector<string>temp, vector<vector<string>>&res){
+        if(row == size){
             res.emplace_back(temp);
             return;
         }
 
-        for(int col = 0 ; col < len ; col++){
-            if(valid(temp,row,col,len)){
+        for(int col = 0; col < size; col++){
+            if(check(temp,row,col)){
                 temp[row][col] = 'Q';
-                solve(row+1,len,res,temp);
+                solve(row+1,size,temp,res);
                 temp[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>res;
-        vector<string>temp(n,string(n,'.'));
+        vector<string>temp(n, string(n,'.'));
 
-        solve(0,n,res,temp);
+        solve(0,n,temp,res);
 
         return res;
     }
