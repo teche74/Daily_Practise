28b3a9d2982bfb1869b6a6212126d6beedc3dc36
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-07 13:28:56 +0530

    Time: 247 ms (59.75%), Space: 148.7 MB (57.71%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    unordered_map<int,unordered_map<int,int>>dp;
-    int solve(int index, vector<int>&prices, int action){
+    int solve(int index, vector<int>&prices, int action, vector<vector<int>> &dp){
         if(index >= prices.size() || action == 0)
         {
             return 0;
         }
 
-        if(dp[index].count(action)) return dp[index][action];
+        if(dp[index][action] != -1) return dp[index][action];
 
         int take =  0, not_take = 0;
 
         if(!(action & 1)){
-            take = -prices[index] + solve(index+1,prices, action-1);
+            take = -prices[index] + solve(index+1,prices, action-1,dp);
         }
         else{
-            take =  prices[index] + solve(index+1,prices,action-1);
+            take =  prices[index] + solve(index+1,prices,action-1,dp);
         }
-        not_take = solve(index+1,prices, action);
+        not_take = solve(index+1,prices, action,dp);
 
         return dp[index][action] = max(take, not_take);
     }
     int maxProfit(vector<int>& prices) {
-        return solve(0,prices,4);
+        vector<vector<int>>dp(prices.size()+1,vector<int>(5,-1));
+        return solve(0,prices,4,dp);
     }
 };
