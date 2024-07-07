36332c4f73e431df9c16f5153061b8ec3a80bce0
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-07 13:08:21 +0530

    Time: 33 ms (50.82%), Space: 20.6 MB (29.87%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,78 +1,67 @@
 class Solution {
 public:
-    void FindRight(vector<int> & arr, int temp[ ]){
-        int size = arr.size();
-
-        stack<int>st;
-
-
-        for(int i =size-1; i >= 0 ; i--){
-            
-            while(!st.empty()  && arr[st.top()] >= arr[i]){
-                st.pop();
-            }
-
-            if(st.empty()) temp[i] = arr.size();
-            else temp[i] = st.top();
-
-            st.push(i);
-        }
-    }
-
-
-    void FindLeft(vector<int>arr, int temp[ ]){
-        int size = arr.size();
-
-        stack<int>st;
-
-
-        for(int i =0; i < size ; i++){
-            
-            while(!st.empty()  && arr[st.top()] >= arr[i]){
-                st.pop();
-            }
-
-            if(st.empty()) temp[i] = -1;
-            else temp[i] = st.top();
-
-            st.push(i);
-        }
-    }
-
-    int MAH(vector<int>arr){
-        int size = arr.size();
-
-        int NGL[size] , NGR[size];
-
-
-        FindRight(arr,NGR);
-        FindLeft(arr,NGL);
-
-        int res =0;
-
-        for(int i =0; i<size; i++){
-            int val = (NGR[i] - NGL[i] - 1) * arr[i];
-            res = max(res,val); 
-        }
-
-        return res;
-    }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int size = matrix[0].size();
-        vector<int>arr(size,0);
+        if (matrix.empty()) return 0;
+
+        vector<int> arr(matrix[0].size(), 0);
         int res = 0;
 
-        for(auto t : matrix){
-            for(int i =0; i < t.size() ;i++){
-                arr[i] += t[i] == '1' ? 1 : 0; 
-                if(t[i] == '0'){
+        auto calculate = [&](vector<int>& arr) {
+            int size = arr.size();
+            vector<int> smaller_left(size), smaller_right(size);
+
+            auto calc_right = [&]() {
+                stack<int> st;
+                for (int i = size - 1; i >= 0; i--) {
+                    while (!st.empty() && arr[st.top()] >= arr[i]) {
+                        st.pop();
+                    }
+                    if (st.empty()) {
+                        smaller_right[i] = arr.size();
+                    } else {
+                        smaller_right[i] = st.top();
+                    }
+                    st.push(i);
+                }
+            };
+
+            auto calc_left = [&]() {
+                stack<int> st;
+                for (int i = 0; i < size; i++) {
+                    while (!st.empty() && arr[st.top()] >= arr[i]) {
+                        st.pop();
+                    }
+                    if (st.empty()) {
+                        smaller_left[i] = -1;
+                    } else {
+                        smaller_left[i] = st.top();
+                    }
+                    st.push(i);
+                }
+            };
+
+            calc_right();
+            calc_left();
+
+            int res = 0;
+            for (int i = 0; i < size; i++) {
+                int val = smaller_right[i] - smaller_left[i] - 1;
+                res = max(res, val * arr[i]);
+            }
+            return res;
+        };
+
+        for (auto& t : matrix) {
+            for (int i = 0; i < t.size(); i++) {
+                if (t[i] == '0') {
                     arr[i] = 0;
+                } else {
+                    arr[i] += (t[i] - '0');
                 }
             }
-            int val = MAH(arr);
-            res = max(res,val);
+            res = max(res, calculate(arr));
         }
+
         return res;
     }
 };
