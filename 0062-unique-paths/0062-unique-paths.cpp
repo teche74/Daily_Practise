f20c2c13850e3c4fda522727d85f05565cff8904
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-09 15:49:25 +0530

    Time: 2 ms (42.81%), Space: 7.2 MB (87.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
+    int dp[101][101];
+    int solve(int sx, int sy, int ex, int ey){
+        if(sx == ex-1  && sy == ey-1) return 1;
+
+        if(dp[sx][sy] != -1) return dp[sx][sy];
+
+        if(sx < 0  ||  sx >= ex || sy < 0 || sy >= ey) return 0;
+
+        return dp[sx][sy] = solve(sx+1,sy,ex,ey) + solve(sx , sy+1,ex,ey);
+
+    }
     int uniquePaths(int m, int n) {
-        int dp[m][n];
-        memset(dp, 0, sizeof(dp));
-
-        // pura right jane ka sirf 1 i tareeka he (from 0 position);
-
-        for(int i = 0; i < m ;i++ ){
-            dp[i][0] =1;
-        }   
-
-        //  pura down jane ka sirf ek tareeka (from 0 position)
-
-        for(int i=0; i < n ; i++){
-            dp[0][i] = 1;
-        }
-
-        for(int i=1; i<m;i++){
-            for(int j =1; j<n;j++){
-                // ab kisi bhi raste ke liye check krna he to simple uske right jane ke kitne tareeke  + down jane ke kitne tarreeke .
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m,n);
     }
 };
