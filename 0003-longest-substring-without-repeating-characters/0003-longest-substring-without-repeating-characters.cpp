18d9429ee8a087ce18f9a55955eabe060c39bde9
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-10 16:01:48 +0530

    Time: 18 ms (42.71%), Space: 11.3 MB (44.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,61 +1,22 @@
 class Solution {
 public:
-    int set_appraoch(string & str){
-        set<char>st;
-
-        int low = 0 , high = 0 , size = str.size() , res= 0 ;
-
-        while(high < size){
-            if(st.find(str[high]) == st.end()){
-                st.insert(str[high++]);
-                res = max(res, high - low);
-            }
-            else{
-                st.erase(str[low++]);
-            }
-        }
-
-        return res;
-    }
-
-    int sliding_approach(string & s){
-        int low = 0, high = 0 , size = s.size() , res = 0;
-
-        unordered_map<char , int>map;
-
-        while(high < size){
-            map[s[high]]++;
-
-            while(map[s[high]] > 1){
-                map[s[low++]]--;
-            }
-            res = max(res, high - low + 1);
-            high++;
-        }
-        return res;
-    }
-
-    int frequency_array(string s){
-        int freq[128];
-
-        memset(freq, -1, sizeof(freq));
-
-        int low = 0 , high = 0 , size = s.size() , res = 0;
-
-        while(high < size){
-            low = max(low, freq[s[high]]+1);
-            res = max(res, high - low+1);
-            freq[s[high]] = high;
-            high++;
-        }
-        return res;
-    }
-
     int lengthOfLongestSubstring(string s) {
-        // int res = set_appraoch(s);
-        // int res1 = sliding_approach(s);
-        int res3 = frequency_array(s);
+        int low = 0 , high = 0 , size = s.size() , res = 0 ;
 
-        return res3;
+        unordered_map<int,int>map;
+
+
+        while( high < size) {
+            map[s[high] - 'a']++;
+
+            while(map[s[high]-'a'] > 1){
+                map[s[low] - 'a']--;
+                low++;
+            }
+
+            res = max(res, high - low+1);
+            high++;
+        }
+        return res;
     }
 };
