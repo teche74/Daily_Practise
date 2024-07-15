9483c374653d1b7f8a121b0e63b4ce317fe6a0fe
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-15 09:43:39 +0530

    Time: 96 ms (43.7%), Space: 57.1 MB (54.89%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool palli(const string & str, int low , int high){
-        while(low<high && str[low] == str[high]){
+    bool palli(const string & ptr, int low, int high){
+
+        while(low <= high){
+            if(ptr[low] != ptr[high]) return false;
             low++;
             high--;
         }
-
-        return low >=high;
+        return true;
     }
-    void solve(int index, const string  & s, vector<string> & temp, vector<vector<string>>&res){
+    void solve(int index,const string s, vector<vector<string>> &res , vector<string> & temp){
         if(index >= s.size()){
             res.emplace_back(temp);
             return;
         }
 
-        for(int i = index ; i < s.size() ; i++){
-            if(palli(s,index,i)){
+        for(int i = index; i < s.size() ;i++){
+            if(palli(s, index, i)){
                 temp.emplace_back(s.substr(index,i-index+1));
-                solve(i+1,s,temp,res);
+                solve(i+1,s,res,temp);
                 temp.pop_back();
             }
         }
@@ -26,7 +27,7 @@ public:
         vector<vector<string>>res;
         vector<string>temp;
 
-        solve(0,s,temp,res);
+        solve(0, s, res, temp);
 
         return res;
     }
