1f71972fbc71080880fe5071dea12344ee8c67af
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-16 08:47:23 +0530

    Time: 6 ms (71.8%), Space: 27.3 MB (11.72%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
-        // brute force
-        // int size = nums.size(), res = 0;
-        // for(int i =0 ; i < size ; i++){
-        //     for(int j = i+1; j <size; j++){
-        //         if( abs(nums[i] - nums[j])  <= min(nums[i], nums[j])){
-        //             res = max(res, nums[i] ^ nums[j]);
-        //         }
-        //     }
-        // }
-        // return res;
+        int size = nums.size(),res = 0; 
 
-
-        // use silding window
-        sort(nums.begin(), nums.end());
-        int size = nums.size(), high = 0, low = 0 ,res = INT_MIN;
-
-        while(high < size){
-            
-            while(high < nums.size() && abs(nums[low] - nums[high]) <= min(nums[low], nums[high])){
-                res = max(res, nums[low] ^ nums[high]);
-                high++;
+        for(int i = 0 ;i  <size; i++){
+            for(int j = i ; j < size; j++){
+                if(abs(nums[i] - nums[j]) <= min(nums[i] ,nums[j]) )
+                    res = max(res, nums[i] ^ nums[j]);
             }
-            low++;
-            high = low+1;
-
         }
-
         return res;
     }
 };
