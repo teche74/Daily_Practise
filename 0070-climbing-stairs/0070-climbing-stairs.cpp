cb42090c8b9bcd7023f62478237a78e561062767
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-17 18:51:52 +0530

    Time: 0 ms (100%), Space: 7.4 MB (37.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,67 +1,25 @@
-// using recursive 
-
-// class Solution {
-// public:
-//     int climbStairs(int n) {
-//         if(n <= 0) return 1;
-
-//         int take_one_step = n > 0  ?climbStairs(n-1) : 0;
-//         int take_two_steps = n > 1 ? climbStairs(n-2) : 0;
-
-//         return take_one_step + take_two_steps; 
-//     }
-// };
-
-// using memoization
-
-// class Solution {
-// public:
-//     unordered_map<int,int>dp;
-//     int climbStairs(int n) {
-//         if(n <= 0) return 1;
-
-//         if(dp.count(n)) return dp[n];
-
-//         int take_one_step = n > 0  ?climbStairs(n-1) : 0;
-//         int take_two_steps = n > 1 ? climbStairs(n-2) : 0;
-
-//         return dp[n] = take_one_step + take_two_steps; 
-//     }
-// };
-
-// using tabulation
-
-// class Solution {
-// public:
-//     int climbStairs(int n) {
-//         int dp[n+1];
-//         memset(dp, 0 ,sizeof(dp));
-
-//         dp[0] = 1;
-//         dp[1] = 1;
-
-//         for(int i =2;i <= n; i++){
-//             dp[i] = dp[i-1] + dp[i-2];
-//         } 
-//         return dp[n];
-//     }
-// };
-
-
-// tabulation -> improved  = spoce optimization
-
 class Solution {
 public:
+    int dp[46];
+    int solve(int curr){
+        if(curr == 0){
+            return 1;
+        }
+
+        if(dp[curr] != -1) return dp[curr];
+
+
+        int take = solve(curr-1);
+
+        int take_2 =0;
+        
+        if(curr >=2)
+            take_2 = solve(curr-2);
+
+        return dp[curr] = take + take_2;
+    }
     int climbStairs(int n) {
-        int prev = 1, curr =1;
-
-        for(int i=2;i<=n;i++){
-            int val = prev+curr;
-            prev =curr;
-            curr = val;
-        } 
-
-        return curr;
-
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
