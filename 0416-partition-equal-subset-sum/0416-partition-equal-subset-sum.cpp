5b990b3c1781a086e969aa5c5636708a8cdfd90f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-17 22:51:59 +0530

    Time: 184 ms (44.66%), Space: 29.9 MB (52.57%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
+    int dp[201][20010];
+    bool solve(int index, vector<int> & nums, int sum){
+        if(sum == 0) return true;
 
+        if(index >= nums.size()){
+            return false;
+        }
+
+        if(dp[index][sum] != -1) return dp[index][sum];
+
+        int take= false, not_take =false;
+
+        if(sum - nums[index] >= 0){
+            take = solve(index+1,nums, sum-nums[index]);
+        }
+        not_take = solve(index+1,nums,sum);
+
+        return dp[index][sum] = take | not_take;
+    }
     bool canPartition(vector<int>& nums) {
-        int sum = ::accumulate(nums.begin(),nums.end(),0);
+        int sum = ::accumulate(nums.begin(), nums.end(),0);
+        int size = nums.size();
 
-        if(sum & 1) return false;
+        if( sum & 1 ) return false;
+        memset(dp,-1,sizeof(dp));
 
         int half = sum >> 1;
 
-        vector<bool>dp(half+1,false);
-        dp[0] = true;
-
-        for(int x : nums){
-            for(int i = half ; i >= x ; i--){
-                dp[i] = dp[i] || dp[i-x];
-            }
-        }
-        return dp[half];
+        return solve(0,nums, half);
     }
 };
