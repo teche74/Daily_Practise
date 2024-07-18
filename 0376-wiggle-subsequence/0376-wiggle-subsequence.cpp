e7e1ac7b4b7844ea919c9a440ea64007cf8be354
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-18 08:01:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -3,17 +3,20 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
         int size = nums.size();
 
-        int inc= 1  , dec = 1;
+        int prev_diff = 0 , curr_diff = 0, count = 1;
 
-        for(int i =0 ; i <size-1; i++){
-            if(nums[i] > nums[i+1] ){
-                inc = dec +1;
-            }
-            else if(nums[i] < nums[i+1]){
-                dec = inc+1;
-            }
-        }
+        for(int i = 0; i < size-1 ; i++){
+            curr_diff = nums[i] - nums[i+1];
 
-        return max(inc,dec);
+            if(curr_diff >= 0 && prev_diff < 0 ){
+                prev_diff = curr_diff;
+                count++;
+            } 
+            else if(curr_diff < 0 && prev_diff >= 0){
+                prev_diff =  curr_diff;
+                count++;
+            }
+        } 
+        return count;
     }
 };
