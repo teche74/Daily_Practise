3d188f530cfc29356bfeed0dbad5aaca32e6c58c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-19 21:52:17 +0530

    Time: 3 ms (97.44%), Space: 14.3 MB (87.39%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        if (nums.empty()) return 0;
-
         int size = *max_element(nums.begin(), nums.end());
-        
-        vector<int> points(size + 1, 0);
-        
-        for (int num : nums) {
-            points[num] += num;
+
+        int arr[size+1];
+
+        memset(arr, 0, sizeof(arr));
+
+        for(int x : nums){
+            arr[x] += x;
         }
 
-        vector<int> dp(size + 1, 0);
-        dp[0] = points[0];
-        dp[1] = max(points[0], points[1]);
 
-        for (int i = 2; i <= size; ++i) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + points[i]);
+        vector<int>dp(size+1, 0);
+
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
+
+
+        for(int i= 2; i<= size; i++){
+            dp[i] = max(dp[i-1] , arr[i] + dp[i-2]);
         }
 
-        return dp[size];
+        return dp[size]; 
     }
 };
