7aaa05a9cfa1d6327816180ca4047a0bdd452193
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-21 18:59:34 +0530

    Time: 7 ms (81.85%), Space: 15.9 MB (28.34%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,20 +3,17 @@ public:
     string convert(string s, int numRows) {
         int size = s.size();
 
-        if(numRows == 1) return s;
+        if( numRows == 1 || s.size() < numRows ) return s;
 
-        if(size  < numRows) return s;
+        vector<string>st(size);
 
-        vector<char>store[numRows];
-
-        int  row = 0 , dir = -1;
+        int row = 0 , dir = -1;
 
         for(char ch : s){
-            store[row].push_back(ch);
-
+            st[row].push_back(ch);
 
             if(row == 0 || row == numRows-1){
-                dir*=-1;
+                dir *= -1;
             }
 
             row+=dir;
@@ -24,11 +21,11 @@ public:
 
         string res = "";
 
-        for(auto s : store){
-            for(auto ch  : s){
-                res.push_back(ch);
-            }
+        for(auto t : st){
+            res+=t;
         }
+
         return res;
+
     }
 };
