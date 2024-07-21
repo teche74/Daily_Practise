83dd02a6aab8226d99918a530c4932a91449e48a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-21 18:46:22 +0530

    Time: 29 ms (36.54%), Space: 94.8 MB (48.32%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int size = nums1.size() + nums2.size();
+        int size = nums1.size()  + nums2.size();
 
-        int prev = 0 ,  curr = 0 , i =0 , j =0; 
+        int first = 0 , second = 0 , i =0 , j = 0;
 
-        for(int count = 0 ; count<= size/2 ; count++){
-            prev = curr;
+        for(int count = 0 ;count <= size/2; count++){
+            first = second;
 
-            if(i < nums1.size()  && j < nums2.size()){
+            if(i < nums1.size() && j < nums2.size()){
                 if(nums1[i] <= nums2[j]){
-                    curr = nums1[i++];
+                    second = nums1[i++]; 
                 }
                 else{
-                    curr = nums2[j++];
+                    second = nums2[j++];
                 }
             }
-            else if( i < nums1.size()){
-                curr = nums1[i++];
+            else if(i < nums1.size()){
+                second = nums1[i++];
             }
             else{
-                curr = nums2[j++];
+                second = nums2[j++];
             }
         }
-
-        if(!(size & 1)){
-            return (prev + curr)/2.0;
+        
+        if(size & 1){
+            return second;
         }
-        return curr;
+        return (first + second)/2.0;
     }
 };
