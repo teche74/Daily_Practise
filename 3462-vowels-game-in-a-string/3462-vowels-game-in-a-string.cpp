acf45aca59d40e0491e915eb30b316a2f5b0b982
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-21 10:39:56 +0530

    Time: 38 ms (100%), Space: 17.5 MB (20%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int vowels  = 0 ;
+
+        for(char ch : s){
+            if(ch == 'a' || ch == 'e' || ch =='o' || ch == 'i' || ch =='u'){
+                return true;
+            }
+        }
+        return false;
+    }
+};
