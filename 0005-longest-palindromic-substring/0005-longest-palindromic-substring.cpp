d38bc4778e387b00b3dfb3d706a75a38ab88d6f5
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-21 18:56:07 +0530

    Time: 8 ms (92.83%), Space: 8.9 MB (72.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int size = s.size();
+        string temp = "$";
 
-        int ind = 0 , len = 0 ;
-
-        function<int(int,int)> check = [&](int low , int high){
-            while(low >= 0 && high < s.size()  && s[low] == s[high]){
-                low--;
-                high++;
-            }
-            return high - low -1;
-        }  ;
-
-        for(int i = 0 ;i  <size ; i++){
-            int l1 = check(i, i);
-            int l2 = check(i,i+1);
-
-            int max_l = max(l1 , l2);
-
-            if(max_l > len){
-                len = max_l;
-                ind = i - (max_l-1)/2;
-            } 
+        for(char ch : s){
+            temp.push_back('|');
+            temp.push_back(ch);
         }
-        return s.substr(ind, len);
+        temp.push_back('|');
+        temp.push_back('@');
+
+        int size= temp.size();
+
+        int lps[size];
+        memset(lps, 0 ,sizeof(lps));
+        int right = 0 , centre = 0 ; 
+
+        for(int i = 1; i < size-1 ; i++){
+            int mirror = 2 * centre - i;
+
+            if(i < right){
+                lps[i] = min(right - i , lps[mirror]);
+            }
+
+            while( temp[ i + lps[i] +1]  == temp[i - lps[i] -1] ){
+                lps[i]++;
+            }
+
+            if( i + lps[i] > right){
+                centre = i;
+                right = i + lps[i];
+            }
+        }
+
+        int max_centre = 0 , max_len = 0;
+
+        for(int i = 1 ;i < size-1 ; i++){
+            if(max_len < lps[i]){
+                max_len = lps[i];
+                max_centre = i;
+            }
+        }
+
+        return s.substr((max_centre-max_len)/2, max_len);
     }
 };
