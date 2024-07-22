d8bf407643311fd22f7e7b0c33b7163b1e33fdc3
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-22 07:59:01 +0530

    Time: 97 ms (6.14%), Space: 74.9 MB (5.27%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int dp[301][5001];
-    int solve(int index, vector<int> & coins , int val){
+    int dp[301][50001];
+    int solve(int index, vector<int> & coins , int value){
+        if(value == 0){
+            return 1;
+        }
         if(index >= coins.size()){
-            return val == 0 ? 1 : 0;
+            return 0;
         }
 
-        if(dp[index][val] != -1)  return dp[index][val];
+        if(dp[index][value] != -1) return dp[index][value];
+        int take =0;
+        
+        if(coins[index] <= value) take = solve(index,coins, value- coins[index]);
 
-        int take = 0, not_take = 0;
+        int not_take = solve(index+1, coins, value);
 
-        if(coins[index] <= val)
-            take = solve(index, coins,val - coins[index]);
+        return dp[index][value] = take + not_take;
 
-        not_take = solve(index+1,coins,val);
-
-        return dp[index][val] = take + not_take; 
     }
     int change(int amount, vector<int>& coins) {
         memset(dp, -1, sizeof(dp));
-        return solve(0, coins, amount);
+        return solve(0 , coins, amount);
     }
 };
