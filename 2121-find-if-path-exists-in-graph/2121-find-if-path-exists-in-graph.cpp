92afd179594c94a0cccc21d61c5f716f657897da
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-23 16:47:52 +0530

    Time: 2492 ms (7.53%), Space: 411.8 MB (7.8%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        if (source == destination) return true;
-
         unordered_map<int,vector<int>>map;
 
-        for(auto t  : edges){
-            map[t[0]].push_back(t[1]);
-            map[t[1]].push_back(t[0]);
+        int size = edges.size();
+
+        for(auto t : edges)
+        {
+            map[t[0]].emplace_back(t[1]);
+            map[t[1]].emplace_back(t[0]);
         }
-
-
-        bool vis[n];
-        memset(vis,false,n);
 
         queue<int>q;
 
-        q.push(source);
-        vis[source] = true;
+        bool vis[200001];
+        memset(vis,false, sizeof(vis));
 
-        while(q.size()){
-            int top = q.front();
+        q.push(source);
+
+        while(!q.empty()){
+            int front  =q.front();
+            vis[front] = true;
             q.pop();
-            
-            for(auto t : map[top]){
-                if(t == destination) return true;
-                if(!vis[t]){
-                    q.push(t);
-                    vis[t] = true;
+
+            if(front == destination) return true;
+
+            for(int c : map[front]){
+                if(!vis[c]){
+                    q.push(c);
                 }
             }
         }
