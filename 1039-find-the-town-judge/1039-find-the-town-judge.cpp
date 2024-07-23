74731e925c1753d4e5ce58e052cc093d40c8cfdc
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-23 17:02:42 +0530

    Time: 142 ms (33.39%), Space: 72.6 MB (24.53%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        // bool vis[n+1] ;
-        // memset(vis, false, sizeof(vis));
-
-        // unordered_map<int,vector<int>>adj;
-
-        // for(auto t : trust){
-        //     vis[t[0]] = true;
-        //     adj[t[1]].emplace_back(t[0]);
-        // }
-
-        // for(int i = 1 ;i <= n; i++){
-        //     if(!vis[i] && adj[i].size() == n-1) return i;
-        // }
-        // return -1;
-
-        int in[n+1], out[n+1];
-
-        memset(in,0,sizeof(in));
-        memset(out,0,sizeof(out));
+        int in[n+1] , out[n+1];
+        memset(in, 0,sizeof(in));
+        memset(out, 0 ,sizeof(out));
 
         for(auto t : trust){
-            in[t[1]]++;
             out[t[0]]++;
+            in[t[1]]++;
         }
 
-        for(int i =1; i <= n; i++){
-            if(in[i] == n-1 && out[i]== 0) return i;
+        for(int i = 1 ;i <= n ;i++){
+            if(in[i] == n-1 && out[i] == 0) return i;
         }
-
         return -1;
-        
     }
 };
