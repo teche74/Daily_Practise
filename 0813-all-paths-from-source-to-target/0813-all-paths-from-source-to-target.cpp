2ef9b41592eb56aa368a7c42938cc60b1be9178f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-23 17:12:20 +0530

    Time: 14 ms (38.46%), Space: 18 MB (22.91%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    void solve(int start, int end , vector<int>temp, vector<vector<int>>&res,unordered_map<int,vector<int>>&map, vector<bool> & vis){
-        if(start == end)
-        {
-            temp.emplace_back(end);
+    void solve(int ele , vector<vector<int>>&res, vector<int>temp, vector<bool> & vis , vector<vector<int>>&graph ){
+        if(ele == graph.size()-1){
+            temp.push_back(ele);
             res.emplace_back(temp);
-            return;
+            return ;
         }
+        temp.emplace_back(ele);
+        vis[ele] = true;
 
-        // if()
-
-        temp.emplace_back(start);
-        vis[start] = true; 
-        
-        for(auto t : map[start]){
-            if(!vis[t]){    
-                solve(t,end,temp,res,map,vis);
+        for(auto t : graph[ele]){
+            if(!vis[t]){
+                solve(t,res,temp,vis,graph);
             }
         }
-
-        vis[start] = false;
+        temp.pop_back();
+        vis[ele] = false;
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        unordered_map<int,vector<int>>map;
-
-        for(int i = 0; i < graph.size() ; i++){
-            for(auto t : graph[i]){
-                map[i].emplace_back(t);
-            }
-        }
-        int size = graph.size();
+        int size  = graph.size();
 
         vector<bool>vis(size,false);
 
         vector<vector<int>>res;
         vector<int>temp;
 
-        solve(0, size-1 ,temp,res,map,vis);
+        solve(0 , res,temp,vis,graph);
 
         return res;
     }
