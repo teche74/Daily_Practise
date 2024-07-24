e67693c209b1d83cb942beef86a102a5d9c7d183
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-24 08:51:56 +0530

    Time: 270 ms (46.31%), Space: 143.2 MB (33.96%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int low, int mid , int high){
-        vector<int>temp(high - low+1, 0);
-        int i =low, j = mid+1, k = 0;
+    void merge(vector<int> & nums , int low,  int mid, int high ){
+        vector<int>temp(high - low+1,0);
 
-        while(i <= mid && j<=high){
+        int i = low , j = mid+1 , k=0;
+
+        while(i <= mid && j <= high){
             if(nums[i] <= nums[j]){
                 temp[k++] = nums[i++];
             }
@@ -17,24 +18,27 @@ public:
             temp[k++] = nums[i++];
         }
 
-        while(j<=high){
+        while(j <= high){
             temp[k++] = nums[j++];
         }
 
-        for(int i =0; i< k; i++){
-            nums[low+i] = temp[i];
+        for(int i =0; i < k ;i++){
+            nums[i+low] = temp[i];
         }
     }
-    void solve(vector<int>&nums, int low, int high){
+    void solve(vector<int> & nums, int low, int high){
         if(low >= high) return;
+
         int mid = low + (high - low)/2;
 
-        solve(nums, low, mid);
-        solve(nums, mid+1, high);
-        merge(nums, low, mid, high);
+        solve(nums,low, mid);
+        solve(nums, mid+1,high);
+        merge(nums,low,mid, high);
     }
     vector<int> sortArray(vector<int>& nums) {
-        solve(nums,0, nums.size()-1);
+        int size = nums.size();
+
+        solve(nums,0 , size-1);
 
         return nums;
     }
