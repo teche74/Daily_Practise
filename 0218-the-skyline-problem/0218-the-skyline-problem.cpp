9622ac3786cb676945b870118ff90913f441c36d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-26 13:28:35 +0530

    Time: 14 ms (95.3%), Space: 19.5 MB (37.26%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.cpp
+++ 0218-the-skyline-problem/0218-the-skyline-problem.cpp
@@ -1,58 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<pair<int,pair<int,char>>>points;
+        vector<pair<int,pair<int,char>>>vec;
 
         for(auto t : buildings){
-            points.push_back({t[0] , {t[2] , 's'}}); // left,height , start
-            points.push_back({t[1] , {t[2] , 'e'}}); // right,height , end
+            vec.push_back({t[0] , {t[2] , 's'}});
+            vec.push_back({t[1] , {t[2] , 'e'}});
         }
 
-        sort(points.begin(),points.end(),[&](pair<int,pair<int,char>> & a , pair<int,pair<int,char>> & b){
-
-            // ek case ye he ki agr do buildings ka start same he to higher height valid building phle rkhi jayegi 
-            // ek case ye he jab dono building ke end same hon to phle choti building ayegi taki usko consider na kre.
-            // agr meri phli building ka end overlap krega agli building ke start ko (is time hum phle second building ka start lenge)
-            
-
+        sort(vec.begin(), vec.end() , [&](pair<int,pair<int,char>> & a , pair<int,pair<int,char>> & b){
             if (a.first != b.first) {
                 return a.first < b.first;
             } else {
                 if (a.second.second == b.second.second) {
                     if (a.second.second == 's') {
-                        return a.second.first > b.second.first; // for starts, higher height first
+                        return a.second.first > b.second.first; 
                     } else {
-                        return a.second.first < b.second.first; // for ends, lower height first
+                        return a.second.first < b.second.first; 
                     }
                 }
-                return a.second.second == 's'; // if one is start and the other is end, start goes first
+                return a.second.second == 's'; 
             }
         });
 
-        multiset<int> heights;
-        heights.insert(0);
-        int prevHeight = 0;
-        vector<vector<int>> res;
+        multiset<int> cont;
 
-        for (auto t : points) {
-            int x = t.first;
-            int h = t.second.first;
-            char type = t.second.second;
+        int prev = 0;
+        cont.insert(0);
+        vector<vector<int>>res;
 
-            if (type == 's') {
-                heights.insert(h);
-            } else {
-                heights.erase(heights.find(h));
+        for(auto t : vec){
+            if(t.second.second == 's'){
+                cont.insert(t.second.first);
+            }
+            else{
+                cont.erase(cont.find(t.second.first));
             }
 
-            int currentHeight = *heights.rbegin();
-            if (currentHeight != prevHeight) {
-                res.push_back({x, currentHeight});
-                prevHeight = currentHeight;
+            int curr = *cont.rbegin();
+            if(curr != prev){
+                res.push_back({t.first, curr});
+                prev=  curr;
             }
         }
-
         return res;
-
     }
 };
