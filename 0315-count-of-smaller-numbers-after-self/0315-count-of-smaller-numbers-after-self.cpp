9ede5ba3a696a8a2e39ea42f9cd993a9895f79fc
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-28 10:21:17 +0530

    Time: 364 ms (71.13%), Space: 247.5 MB (31.92%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,24 +1,57 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-
-using namespace __gnu_pbds;
-using namespace std;
-
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>ordered_set;
-
 class Solution {
 public:
+    void merge(int low, int mid, int high , vector<pair<int, int>>&nums, vector<int> & res){
+
+        vector<pair<int,int>>temp(high - low+1);
+
+        int i = low  , j = mid+1;
+        int k =0 ;
+
+        while(i <= mid && j <= high){
+            if(nums[i].first <= nums[j].first){
+                temp[k++] = nums[j++];
+            }
+            else{
+                res[nums[i].second] += high - j + 1; 
+                temp[k++] = nums[i++];
+            }
+        }
+
+        while(i<=mid){
+            temp[k++] = nums[i++];
+        }
+
+        while(j<=high){
+            temp[k++] = nums[j++];
+        }
+
+        for(int i =0 ;i < k ; i++){
+            nums[low+ i]  = temp[i];
+        }
+    }
+    void helper(int low, int high,vector<pair<int, int>>& nums, vector<int> & temp){
+        
+        if(low < high){
+            int mid = low + (high - low)/2;
+
+            helper(low, mid, nums,temp);
+            helper(mid+1,high , nums,temp);
+
+            merge(low, mid, high , nums,temp);
+        }
+    }
+
     vector<int> countSmaller(vector<int>& nums) {
         int size = nums.size();
-        vector<int>res(size,0);
+        vector<int>temp(size,0);
+        vector<pair<int, int>> arr;
+        
+	    for(int i = 0; i < size; i++)
+	    {
+	        arr.push_back({nums[i], i});
+	    }
 
-        ordered_set s;
-
-        for(int i =size-1 ; i>=0 ; i--){
-            s.insert(nums[i]);
-            res[i] = s.order_of_key(nums[i]);
-        }
-        return res;
+        helper(0 , size-1, arr,temp);
+        return temp;
     }
 };
