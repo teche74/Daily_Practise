458142e1275b0c8384be55136ab8dcf16f507cd6
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 10:09:08 +0530

    Time: 7 ms (13.24%), Space: 9 MB (23.21%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int myAtoi(string s) {
         int size = s.size();
-
         int index = 0 ;
+        bool sign = false;
+        long long  val = 0;
 
-        while(index < size && isspace(s[index])) index++;
 
-        bool sign  = false;
+        while(index < size && isspace(s[index])) index++; 
+
 
         if(s[index] == '-'){
-            sign  =true;
+            sign = !sign;
             index++;
-        }
+        } 
         else if(s[index] == '+'){
             index++;
         }
 
-        long long val = 0;
-
-        while(index < size && isdigit(s[index])){
+        while(index  < size && isdigit(s[index])){
             val*=10;
             val+=(s[index] - '0');
-            if(val >= INT_MAX || val <= INT_MIN) break;
+            if(val >=  INT_MAX || val <= INT_MIN) break;
             index++;
         }
 
         if(sign) val*=-1;
-
-        if(val >=INT_MAX) return INT_MAX;
+        if(val >= INT_MAX) return INT_MAX;
         if(val <= INT_MIN) return INT_MIN;
-
         return val;
     }
 };
