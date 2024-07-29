dd95f9d4c15c467a0c2f27d9eafdad59e471bb9a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 11:56:13 +0530

    Time: 24 ms (79.22%), Space: 25.2 MB (33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,16 +3,17 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>res;
 
-        unordered_map<string,vector<string>>map;
+        unordered_map<string,vector<string>>store;
 
         for(auto t : strs){
-            string temp = t;
+            string str = t;
+
             sort(t.begin(),t.end());
 
-            map[t].emplace_back(temp);
+            store[t].emplace_back(str);
         }
 
-        for(auto t : map){
+        for(auto t : store){
             res.emplace_back(t.second);
         }
 
