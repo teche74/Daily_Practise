95d159bf9781273ccf57cf24e2977f69b4cf2acc
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 10:19:46 +0530

    Time: 0 ms (100%), Space: 10.3 MB (51.35%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        // unordered_map<char,int>map;
+        int val[ ] = {1000,900 , 500, 400 ,100 ,90 , 50, 40, 10, 9, 5, 4 ,1};
+        string sign[ ]  = {"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" ,"XL" , "X" ,"IX" , "V", "IV", "I"};
 
-        // map['I'] = 1;
-        // map["V"] = 5;
-        // map["X"] = 10;
-        // map["L"] = 50;
-        // map["C"] = 100;
-        // map["D"] = 500;
-        // map["M"] = 1000;
-
-        // int val = 0;
-
-        // for()
-
-        int val[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string mark[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int size = sizeof(val)/sizeof(int);
+        string res= "";
 
-        string res = "";
-
-        for(int i =0 ;i <  size; i++){
+        for(int i = 0 ;i  <size; i++){
             while(num >= val[i]){
-                num-=val[i];
-                res+=mark[i];
+                num -=val[i];
+                res+=sign[i];
             }
-        }
+
+            if(num == 0) break;
+        } 
 
         return res;
     }
