257f563632289b58f2141911b7d9d6f87fd83a99
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 15:11:22 +0530

    Time: 0 ms (100%), Space: 13.8 MB (6.4%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
@@ -11,48 +11,33 @@
  */
 class Solution {
 public:
-    bool same_level(TreeNode* root, int x, int y) {
-        if (!root) return false;
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while (!q.empty()) {
-            int size = q.size();
-            bool got_1 = false, got_2 = false;
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode* front = q.front();
-                q.pop();
-                
-                if (front->val == x) got_1 = true;
-                if (front->val == y) got_2 = true;
-                
-                if (front->left) q.push(front->left);
-                if (front->right) q.push(front->right);
-            }
-            
-            if (got_1 && got_2) return true;
-            if (got_1 || got_2) return false;
-        }
-        
-        return false;
-    }
-    
-    bool solve(TreeNode* root, int x, int y){
-        if(!root) return true;
-
-        bool left = solve(root->left,x,y);
-        bool right = solve(root->right,x,y);
-
-        if(root->left && root->right && (root->left->val == x && root->right->val == y || root->left->val == y && root->right->val == x) ){
-            return false;
-        }
-
-        return left && right;
-    }
     bool isCousins(TreeNode* root, int x, int y) {
-        if(!same_level(root,x,y)) return false;
+        unordered_map<int,pair<TreeNode * , int>>map; // current_value  : < uska parent , uska level>;
 
-        return solve(root, x,y);
+        queue<TreeNode * > q;
+        
+        q.push(root);
+        map[root->val] = {nullptr , 0};
+        int level = 0;
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0;i  < size ; i++){
+                TreeNode * front = q.front();
+                q.pop();
+
+                if(front->left){
+                    q.push(front->left);
+                    map[front->left->val] = {front , level+1}; 
+                }
+                if(front->right){
+                    q.push(front->right);
+                    map[front->right->val] = {front, level+1};
+                }
+            }
+            level++;
+        }
+
+        return !(map[x].first == map[y].first || map[x].second != map[y].second);
     }
 };
