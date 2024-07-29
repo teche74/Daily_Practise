65efd698f6bb53ff51a6bd8afc521a4bb2f9c103
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 11:29:06 +0530

    Time: 4 ms (61.26%), Space: 13.3 MB (32.21%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    void solve(int index, vector<int> & arr, int val , vector<int>temp, vector<vector<int>>&res){
-        if(val == 0){
-            res.emplace_back(temp);
-            return;
-        }
-
-        for(int i =index; i<arr.size() ;i++){
-            if(i > index && arr[i] == arr[i-1]) continue;
-            if(arr[i] > val) break;
-            temp.emplace_back(arr[i]);
-            solve(i+1,arr,val-arr[i],temp,res);
-            temp.pop_back();
-        }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
         vector<vector<int>>res;
         vector<int>temp;
+        int size = cand.size();
 
-        sort(candidates.begin(),candidates.end());
+        sort(cand.begin(),cand.end());
 
-        solve(0, candidates,target,temp,res);
+
+        function<void(int,int)> solve = [&](int index, int val){
+
+            if(val == 0){
+                res.emplace_back(temp);
+                return;
+            }
+
+
+            for(int i =  index ; i < size ; i++){
+                if(i > index && cand[i] == cand[i-1]) continue;
+                if(cand[i] > val) break;
+                temp.emplace_back(cand[i]);
+                solve(i+1,val-cand[i]);
+                temp.pop_back();
+            }
+        };
+
+        solve(0,target);
 
         return res;
     }
