60887d4f19298c7da3b9544508a96737054632ce
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 09:45:33 +0530

    Time: 4 ms (90.52%), Space: 10.9 MB (75.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,71 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // approach 1 : set
+
+        // unordered_set<int>st;
+        // int low =0 , high = 0 ,  size = s.size() ,res = 0;
+
+        // while(high < size){
+        //     if(st.find(s[high]) == st.end()){
+        //         st.insert(s[high++]);
+        //         res = max(res, high - low);
+        //     }
+        //     else{
+        //         st.erase(s[low++]);
+        //     }
+        // }
+
+        // return res;
+
+
+
+
+
+
+        // approach 2 sliding
+
+        // int map[256];
+        // memset(map, 0, sizeof(map));
+
+        // int low = 0 , high = 0 , size = s.size(), res = 0;
+
+        // while(high < size){
+        //     map[s[high]]++;
+            
+        //     while(map[s[high]] > 1){
+        //         map[s[low]]--;
+        //         low++;
+        //     }
+
+        //     res = max(res, high - low+1);
+
+        //     high++;
+        // }
+
+        // return res;
+
+
+
+
+
+
+
+        // 3rd approach  :  modified sliding (last occurence).
+
         int map[256];
         memset(map, -1, sizeof(map));
 
         int low = 0 , high = 0 , size = s.size(), res = 0;
 
-        while(high < size){
-            low = max(low , map[s[high]]+1);
+        while(high  < size){
+            low = max(low , map[s[high]] +1);
             res = max(res, high - low+1);
-            map[s[high]] = high; 
+            map[s[high]] = high;
             high++;
         }
 
         return res;
+
     }
 };
