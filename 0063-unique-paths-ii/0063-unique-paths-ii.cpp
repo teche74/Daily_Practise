55186cba36a61723039fec9b23603ed85dee2255
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 21:43:23 +0530

    Time: 3 ms (53.87%), Space: 11.1 MB (13.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int m = arr.size(), n = arr[0].size();
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+        int rows = mat.size() , cols = mat[0].size();
 
-        if(arr[0][0] == 1 || arr[m-1][n-1] == 1) return 0;
-        
-        vector<vector<int>>dp(m, vector<int>(n,0));
+        if(mat[0][0] == 1 || mat[rows-1][cols-1] == 1) return 0;
 
+        int dp[101][101];
 
-        dp[0][0] = 1;
-
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = (arr[i][0] == 0 && dp[i-1][0] == 1) ? 1 : 0;
-        }
-
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = (arr[0][j] == 0 && dp[0][j-1] == 1) ? 1 : 0;
-        }
-
-        for(int i =1; i < m ; i++){
-            for(int j =1; j<n;j++){
-                if(arr[i][j] != 1){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
+        function<int(int,int)>solve = [&](int row , int col){
+            if(row == rows-1 && col == cols -1){
+                return 1;
             }
-        }
-        return dp[m-1][n-1];
+
+            if( row < 0 || row >= rows || col < 0 || col >= cols || mat[row][col] == 1){
+                return 0;
+            }
+
+            if(dp[row][col] != -1) return dp[row][col];
+
+            int left = solve(row+1,col);
+            int right = solve(row,col+1);
+
+            return dp[row][col] = left + right ; 
+        }; 
+
+        memset(dp, -1, sizeof(dp));
+        return solve(0,0);
     }
 };
