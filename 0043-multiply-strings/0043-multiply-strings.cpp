67630214be63b35f3a47dfa4cb6c9ca3a8053832
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 11:40:21 +0530

    Time: 3 ms (77.98%), Space: 8.4 MB (69.74%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,23 +3,27 @@ public:
     string multiply(string num1, string num2) {
         int m = num1.size() , n = num2.size();
 
-        vector<int>res(m+n, 0);
+        int res[n+m];
+        memset(res, 0, sizeof(res));
 
-        for(int i = m-1 ; i >=0 ;i--){
-            for(int j = n-1; j >= 0 ; j--){
-                int val = (num1[i] - '0') * (num2[j] - '0') + res[i+j+1];
-                res[i+j]  += (val/10);  
-                res[i+j+1] = (val%10);
+        for(int i = m-1 ; i >= 0 ;i --){
+            for(int j = n-1; j >= 0 ;j--){
+                int val = (num1[i] - '0') * (num2[j] - '0');
+
+                val += res[i+j+1];
+                res[i+j+1] = val%10;
+                res[i+j] += val/10;
             }
         }
 
-        string ans = "";
+        string ans = "" ;
 
-        for(int x : res){
-            if( !(x == 0 && ans.empty()) ){
-                ans.push_back(x + '0');
+        for(int i = 0 ;i < n+m ;i++){
+            if(!(ans.empty() && res[i] == 0)){
+                ans.push_back(res[i] + '0');
             }
         }
-        return ans.empty() ? "0" : ans;
+
+        return ans == "" ? "0" : ans;
     }
 };
