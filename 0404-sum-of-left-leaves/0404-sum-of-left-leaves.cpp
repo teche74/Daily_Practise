bef59ef06b1ed0c323862cc905d909bd41e04288
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 19:14:31 +0530

    Time: 5 ms (18.16%), Space: 14.8 MB (42.59%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -11,33 +11,48 @@
  */
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        int sum = 0;
-
-        if(!root) return sum;
-
-        queue<TreeNode*>q;
-
-        q.push(root);
-
-        while(!q.empty()){
-            int size = q.size();
-
-            for(int i = 0 ; i <size ; i++){
-                TreeNode * front = q.front();
-                q.pop();
-
-                if(front->left){
-                    if(!front->left->right && !front->left->left)
-                        sum+=front->left->val;
-
-                    q.push(front->left);
-                }
-                if(front->right){
-                    q.push(front->right);
-                }
+    void solve(TreeNode *root, int  & sum , TreeNode * prev){
+        if(root){
+            if(prev && prev-> left == root && root->left == nullptr  && root->right == nullptr){
+                sum+=root->val;
             }
+            prev =root;
+            solve(root->left,sum,prev);
+            solve(root->right,sum,prev);
         }
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        // int sum = 0;
+
+        // if(!root) return sum;
+
+        // queue<TreeNode*>q;
+
+        // q.push(root);
+
+        // while(!q.empty()){
+        //     int size = q.size();
+
+        //     for(int i = 0 ; i <size ; i++){
+        //         TreeNode * front = q.front();
+        //         q.pop();
+
+        //         if(front->left){
+        //             if(!front->left->right && !front->left->left)
+        //                 sum+=front->left->val;
+
+        //             q.push(front->left);
+        //         }
+        //         if(front->right){
+        //             q.push(front->right);
+        //         }
+        //     }
+        // }
+
+        // return sum;
+
+        int sum = 0 ;
+        solve(root, sum , nullptr);
 
         return sum;
     }
