0d46af4ce987692df4efa929123c9fcc28bf334b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 12:17:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (14.69%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,42 +1,35 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& arr) {
-        vector<int> res;
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        vector<int>res;
 
-        if (arr.empty()) 
-            return res;
+        int top = 0 , right = mat[0].size()-1 , left = 0 , bottom = mat.size()-1;
 
-        int top = 0, right = arr[0].size() - 1, bottom = arr.size() - 1, left = 0;
-
-        while (top <= bottom && left <= right) {
-
-            for (int i = left; i <= right; i++) {
-                res.push_back(arr[top][i]);
+        while(top <= bottom && left <= right){
+            
+            for(int i = left ; i <=right ; i++){
+                res.emplace_back(mat[top][i]);
             }
             top++;
 
-            for (int i = top; i <= bottom; i++) {
-                res.push_back(arr[i][right]);
+            for(int i = top ; i<= bottom ; i++){
+                res.emplace_back(mat[i][right]);
             }
             right--;
 
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    res.push_back(arr[bottom][i]);
+            if(top <= bottom){
+                for(int i = right ; i>= left; i--){
+                    res.emplace_back(mat[bottom][i]);
                 }
                 bottom--;
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    res.push_back(arr[i][left]);
+            if(left <= right){
+                for(int i=bottom ; i>= top ;i--){
+                    res.emplace_back(mat[i][left]);
                 }
                 left++;
             }
         }
-
         return res;
     }
 };
