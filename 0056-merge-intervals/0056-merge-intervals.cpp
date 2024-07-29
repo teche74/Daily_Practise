66a7ff78fa404344b573cd8aaea05ed08cf1634f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 12:25:56 +0530

    Time: 25 ms (46.5%), Space: 22.9 MB (38.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,23 +4,18 @@ public:
         int size = intervals.size();
 
         sort(intervals.begin(), intervals.end());
-
         vector<vector<int>>res;
 
-        res.push_back(intervals[0]);
+        res.emplace_back(intervals[0]);
 
-        for(int i =1; i <size ;){
-            if(res.back()[1] >= intervals[i][0]){
-                int new_end = res.back()[1];
-                while( i < size && new_end >= intervals[i][0]){
-                    new_end = max(new_end , intervals[i][1]);
-                    i++;
-                }
-                res.back()[1] = new_end;
-                if(i == size) break;
+        for(int i= 1; i <size ;){
+            while(i < size && res.back()[1]  >= intervals[i][0]){
+                res.back()[1] = max(res.back()[1],intervals[i][1]);
+                res.back()[0] = min(res.back()[0] , intervals[i][0]);
+                i++;
             }
-            else{
-                res.push_back(intervals[i]);
+            if(i < size && res.back()[1]  <= intervals[i][0]){
+                res.emplace_back(intervals[i]);
                 i++;
             }
         }
