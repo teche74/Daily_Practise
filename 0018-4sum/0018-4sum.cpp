9d1646ac1185b30bd653175138b8477448d835e9
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 10:35:49 +0530

    Time: 32 ms (37.97%), Space: 16.1 MB (44.37%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& arr, int val) {
-        int size = arr.size();
-        sort(arr.begin(),arr.end());
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int size = nums.size();
         vector<vector<int>>res;
-        for(int i =0; i <size;i++){
-            if(i >0 && arr[i] == arr[i-1]) continue;
 
-            for(int j = i+1;j<size ;j++){
-                if( j> i+1 && arr[j] == arr[j-1]) continue;
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0 ;i < size ; i++ ){
+            if( i > 0 && nums[i] == nums[i-1]) continue;
+
+            for(int j = i+1;  j < size; j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
                 int k = j+1;
                 int l = size-1;
 
-                while(k<l){
-                    long  long sum = arr[i];
-                    sum+=arr[j];
-                    sum+=arr[k];
-                    sum+= arr[l];
+                while(k <l){
+                    long long sum = nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
 
-                    if(sum  == val){
-                        vector<int>temp = {arr[i],arr[j],arr[k],arr[l]};
-                        res.push_back(temp);
-
+                    if(sum == target){
+                        res.push_back({nums[i] , nums[j] , nums[k], nums[l]});
                         k++;
                         l--;
 
-                        while(k<l && arr[k] == arr[k-1]) k++;
-                        while(k<l && arr[l] == arr[l+1]) l--;
+                        while(k <l && nums[k] == nums[k-1]) k++;
+                        while(k <l && nums[l] == nums[l+1]) l--;
                     }
-
-                    else if(sum > val){
+                    else if(sum > target){
                         l--;
                     }
                     else{
