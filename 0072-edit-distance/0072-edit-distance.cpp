17a9fd5929a913cd9974cf498bfc4ec0b216df77
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-29 22:21:26 +0530

    Time: 51 ms (14.95%), Space: 11.9 MB (74.62%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.size() , n = word2.size();
+        int m = word1.size(), n = word2.size();
+        int dp[501][501];
 
-        vector<vector<int>>dp(m+1, vector<int>(n+1,0));
-
-        for(int i =0; i<= m; i++){
-            for(int j = 0 ;j <= n ;j++){
-                if(i ==0 || j== 0){
-                    dp[i][j] = (i== 0) ? j : i;
-                }
-                else
-                    dp[i][j] = min(dp[i-1][j]+1,min(dp[i][j - 1] + 1,dp[i - 1][j - 1] + ((word1[i - 1] == word2[j - 1]) ? 0 : 1)));
+        function<int(int,int)>solve = [&](int i , int j){
+            if(i == m){
+                return n - j;
             }
-        }
+            if(j == n){
+                return m - i;
+            }
 
-        return dp[m][n];
-    }
+            if(dp[i][j] != -1) return dp[i][j];
+
+            int no_oper = INT_MAX;
+            if(word1[i] == word2[j]){
+                no_oper = solve(i+1,j+1);
+            }
+            int insert = 1 + solve(i,j+1);
+            int del  = 1 + solve(i+1,j);
+            int replace =1 + solve(i+1,j+1);
+
+            return dp[i][j] = min({insert,del,replace, no_oper});
+        };
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0);
+    };
 };
