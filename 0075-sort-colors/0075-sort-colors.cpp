e24fb7ca7fc0b438221410dcbe4a8909d82c7809
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-30 09:03:31 +0530

    Time: 0 ms (100%), Space: 10 MB (14.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,29 +2,49 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        function<void(void)> approach1 = [&](){
-            int count_z = 0 , count_one = 0 ,size = nums.size();
+        // function<void(void)> approach1 = [&](){
+        //     int count_z = 0 , count_one = 0 ,size = nums.size();
 
-            for(int i = 0; i < size; i++){
-                count_z +=(nums[i] == 0);
-                count_one  += (nums[i] == 1);
+        //     for(int i = 0; i < size; i++){
+        //         count_z +=(nums[i] == 0);
+        //         count_one  += (nums[i] == 1);
+        //     }
+
+        //     for(int i =0; i < nums.size(); i++){
+        //         if(count_z > 0){
+        //             nums[i] = 0;
+        //             count_z--;
+        //         }
+        //         else if(count_one > 0){
+        //             nums[i] = 1;
+        //             count_one--;
+        //         }
+        //         else{
+        //             nums[i] = 2;
+        //         }
+        //     }
+        // };
+
+        // approach1();
+
+
+        // dutch national flag algo
+
+        int low = 0 , mid = 0 , high = nums.size()-1;
+
+        while(mid<= high){
+            if(nums[mid] == 1){
+                mid++;
             }
-
-            for(int i =0; i < nums.size(); i++){
-                if(count_z > 0){
-                    nums[i] = 0;
-                    count_z--;
-                }
-                else if(count_one > 0){
-                    nums[i] = 1;
-                    count_one--;
-                }
-                else{
-                    nums[i] = 2;
-                }
+            else if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
             }
-        };
-
-        approach1();
+            else{
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
+        } 
     }
 };
