d486e682fd32c6145dd2a6e284be4334afb23798
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-07-31 08:36:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (34.08%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
     int dp[101];
-    int solve(int index, const string & s){
+    int solve(int index, const string  & s){
         if(index >= s.size()){
             return 1;
         }
 
-
         if(s[index] == '0') return 0;
-        if(dp[index] != -1) return dp[index];
 
-        int one_dig = 0 , two_dig =0;
+        if(dp[index] != -1){
+            return dp[index];
+        }
+
+        int single_digit = 0, double_digit = 0;
+
+        single_digit = solve(index+1,s);
 
         if(index+1 < s.size()){
-            string ptr = s.substr(index,2);
-            if(stoi(ptr) <= 26 && stoi(ptr) >= 10){
-                two_dig  = solve(index+2,s);
+            string str = s.substr(index,2);
+            if(str >= "1" && str <= "26"){
+                double_digit = solve(index+2,s);
             }
         }
 
-        one_dig = solve(index+1,s);
-
-        return dp[index] = one_dig + two_dig;
+        return dp[index] = single_digit + double_digit; 
     }
     int numDecodings(string s) {
-        // memset(dp,-1,sizeof(dp));
-        // return solve(0,s);
-
-        int size = s.size();
-
-        int dp[size+1];
-
-        dp[size] = 1;
-
-        for(int i = size-1; i>= 0 ; i--){
-
-            if(s[i] == '0'){
-                dp[i] = 0;
-            }
-            else
-            {
-                dp[i] = dp[i+1];
-
-                if(i+1 < size && (s[i] == '1' || s[i] == '2' && s[i+1] >= '0' && s[i+1] <= '6') ){
-                    dp[i] += dp[i+2];
-                }
-            }
-        }
-        return dp[0];
+        memset(dp,-1,sizeof(dp));
+        return solve(0 , s);
     }
 };
