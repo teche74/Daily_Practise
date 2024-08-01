3bafb03af177b5697e7ec028b7395df6ee1c663c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-01 07:12:40 +0530

    Time: 5 ms (49.66%), Space: 9.3 MB (54.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,12 @@ public:
         vector<int>res(n+1,0);
 
         for(int i = 1; i <= n ; i++){
-            res[i] = res[i >> 1] + (i & 1);
+            //  half the bit  && aloso check if it is odd;
+            res[i] = res[i/2];
+
+            if(i & 1){
+                res[i]++;
+            }
         }
         return res;
     }
