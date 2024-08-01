477f781c6918b5e25f97c92016b964a4cdeac715
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-01 07:10:28 +0530

    Time: 2 ms (75.85%), Space: 9.3 MB (54.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>dp(n+1,0);
+        vector<int>res(n+1,0);
 
-        for(int i= 1; i<= n; i++){
-            if(!(i & 1)){
-                dp[i] = dp[i/2];
-            }
-            else{
-                dp[i] = dp[i/2]+1;
-            }
+        for(int i = 1; i <= n ; i++){
+            res[i] = res[i >> 1] + (i & 1);
         }
-        return dp; 
+        return res;
     }
 };
