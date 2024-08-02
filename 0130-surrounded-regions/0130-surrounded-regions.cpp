3ee2c92e88a7b7f8baf38f150b13921c8551ec23
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-02 13:24:32 +0530

    Time: 7 ms (92.29%), Space: 14.8 MB (18.01%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int rows= board.size() , cols = board[0].size();
+        int rows = board.size() , cols = board[0].size() ;
 
-        vector<vector<int>>dirs = {{0,1},{0,-1},{1,0} , {-1,0}};
+        vector<vector<int>>dirs  = {{0,1}, {1,0} , {-1,0} , {0,-1}};
 
-        function<void(int, int)> dfs = [&](int row, int col) {
-            if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != 'O') {
-                return;
-            }
-            board[row][col] = '#'; 
-            for (auto dir : dirs) {
-                int new_r = row + dir[0];
-                int new_c = col + dir[1];
-                dfs(new_r, new_c);
+        function<void(int,int)>dfs = [&](int row , int col){
+            if(row < 0 || row >= rows || col < 0  || col >= cols || board[row][col] != 'O') return;
+
+            board[row][col] = '#';
+
+            for(auto t : dirs){
+                int new_r = row + t[0];
+                int new_c = col + t[1];
+
+                dfs(new_r,new_c); 
             }
         };
 
-        for (int i = 0; i < rows; ++i) {
-            if (board[i][0] == 'O') dfs(i, 0);
-            if (board[i][cols - 1] == 'O') dfs(i, cols - 1);
-        }
-        for (int j = 0; j < cols; ++j) {
-            if (board[0][j] == 'O') dfs(0, j);
-            if (board[rows - 1][j] == 'O') dfs(rows - 1, j);
-        }
+        for(int i = 0 ; i <rows ; i++){
+            for(int j =0; j <cols ; j++){
+                if((i == 0 || i ==rows-1 || j == 0 || j == cols -1) && board[i][j] == 'O'){
+                    dfs(i,j);
+                }
+            }
+        } 
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (board[i][j] == 'O') {
+        for(int i = 0; i<rows ;i++){
+            for(int j = 0 ; j <cols ; j++){
+                if(board[i][j] == 'O'){
                     board[i][j] = 'X';
-                } else if (board[i][j] == '#') {
+                }
+                else if(board[i][j] == '#'){
                     board[i][j] = 'O';
                 }
             }
         }
+
+
     }
 };
