eacec077eb000bd82030fa5fb5949c531348243c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-03 21:58:21 +0530

    Time: 4 ms (88.86%), Space: 13.4 MB (56.63%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void solve(int ele , vector<vector<int>>&res, vector<int>temp, vector<bool> & vis , vector<vector<int>>&graph ){
-        if(ele == graph.size()-1){
-            temp.push_back(ele);
-            res.emplace_back(temp);
-            return ;
-        }
-        temp.emplace_back(ele);
-        vis[ele] = true;
-
-        for(auto t : graph[ele]){
-            if(!vis[t]){
-                solve(t,res,temp,vis,graph);
-            }
-        }
-        temp.pop_back();
-        vis[ele] = false;
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int size  = graph.size();
-
-        vector<bool>vis(size,false);
-
         vector<vector<int>>res;
         vector<int>temp;
+        vector<bool>vis(graph.size(),false);
+        temp.emplace_back(0);
+        vis[0]  =true;
+        int end = graph.size()-1;
 
-        solve(0 , res,temp,vis,graph);
+        function<void(int)>solve = [&](int curr){
+            if(curr ==  end){
+                res.emplace_back(temp);
+                return;
+            }
 
+
+            for(auto t : graph[curr]){
+                if(!vis[t]){
+                    vis[t] = true;
+                    temp.emplace_back(t);
+                    solve(t);
+                    temp.pop_back();
+                    vis[t] =false;
+                }
+            }
+        }; 
+        
+        solve(0);
         return res;
     }
 };
