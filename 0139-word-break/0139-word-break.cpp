bb9ad4447462d62179c8ec25550fd8650cd9856b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-03 14:59:07 +0530

    Time: 0 ms (100%), Space: 12.9 MB (62.15%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,67 +1,66 @@
-struct node{
-    node * links[26];
-    bool end = false;
+struct Node{
+    Node * links[26];
+    bool end =false;
 
-    void CreateRef(char ch, node * n){
+
+    void CreateRef(char ch, Node * n){
         links[ch - 'a'] = n;
-    }
+    } 
 
-    node * GetRef(char ch){
+    bool CheckRef(char ch){
         return links[ch - 'a'];
     }
 
-    bool CheckRef(char ch){
-        return links[ch - 'a'] != nullptr;
+    Node * GetRef(char ch){
+        return links[ch - 'a'];
+    }
+
+    bool CheckEnd(){
+        return end;
     }
 
     void SetEnd(bool val){
         end = val;
     }
-
-    bool IsEnd(){
-        return end;
-    }
-    
 };
 
 class Solution {
 public:
-    node * root = new node();
-    unordered_map<int,bool> dp;
-    bool wordBreak(string s, vector<string>& wordDict) {
-        auto Init = [&](){
-            for(const string str : wordDict){
-                node * temp = root;
-                for(char ch : str){
-                    if(!temp->CheckRef(ch)){
-                        temp->CreateRef(ch , new node());
-                    }
-                    temp = temp->GetRef(ch);
-                }
-                temp->SetEnd(true);
+    unordered_map<int,bool>map;
+    Node * root = new Node();
+
+    bool solve(int index, const string & str, Node  *temp ){
+        if(index == str.size()) return true;
+
+        if(map.count(index))return map[index];
+
+        for(int i = index ;i < str.size() ; i++){
+            if(!temp->CheckRef(str[i])){
+                break;
             }
-        };
-        
-        Init();
-
-
-        function<bool(int , node *)> solve = [&](int index, node  *temp){
-            if(index >= s.size()) return true;
-
-            if(dp.count(index)) return dp[index];
-
-            for(int i = index ; i < s.size(); i++){
-                if(!temp->CheckRef(s[i])) break;
-                temp = temp->GetRef(s[i]);
-                if(temp->IsEnd() && solve(i+1,root) == true){
-                    return dp[index] = true;
-                }
+            temp = temp->GetRef(str[i]);
+            if(temp->CheckEnd() && solve(i+1,str,root)){
+                map[index] = true;
+                return true;
             }
-            return dp[index] = false;
-        };
-
-        node * temp = root;
-        return solve(0,temp);
+        }
+        return map[index] = false;
     }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        for(string  &str : wordDict){
+            Node * temp = root;
 
+            for(char ch : str){
+                if(!temp->CheckRef(ch)){
+                    temp->CreateRef(ch , new Node());
+                }
+                temp = temp->GetRef(ch);
+            }
+            temp->SetEnd(true);
+        }   
+
+        Node *temp = root;
+        return solve( 0, s, temp);
+    }
 };
