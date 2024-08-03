061b0702f304186a51e2fe8fa5131971edb2a246
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-03 22:14:05 +0530

    Time: 384 ms (5.02%), Space: 211.8 MB (5.02%) - LeetHub

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int>in(n,0);
+        // # no of components 
 
-        for(auto t : edges){
-            in[t[1]]++;
+        unordered_map<int,vector<int>>map;
+
+        for (const auto& edge : edges) {
+            map[edge[0]].emplace_back(edge[1]);
         }
 
-        vector<int>res;
+        set<int>vals;
+        vector<bool>vis(n,false);
 
-        for(int i = 0; i < n; i++){
-            if(in[i] == 0){
-                res.emplace_back(i);
+        function<void(int)> bfs = [&](int curr){
+            queue<int>q;
+
+            q.push(curr);
+
+            while(!q.empty()){
+                int front = q.front();
+                q.pop();
+
+                for(auto t : map[front]){
+                    if(!vis[t]){
+                        if(vals.find(t) != vals.end()){
+                            vals.erase(t);
+                        }
+                        vis[t] = true;
+                        q.push(t);
+                    }
+                }
+            }
+        };
+
+        for(int i = 0; i < n ; i++){
+            if(!vis[i]){
+                vals.insert(i);
+                bfs(i);
             }
         }
-        return res;
+
+        return {vals.begin(),vals.end()};
     }
 };
