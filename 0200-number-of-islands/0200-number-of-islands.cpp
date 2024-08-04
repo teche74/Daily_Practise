8b9bc10fc1ef0f9c17544cbfd547aef4f606aceb
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-04 16:25:23 +0530

    Time: 93 ms (6.51%), Space: 29.7 MB (7.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size() , cols = grid[0].size();
-        vector<vector<bool>>vis(rows, vector<bool>(cols, false));
-
-        vector<vector<int>>dirs = {{1,0}, {0,1} , {-1,0}, {0,-1}};
-
-        function<void(int,int)> dfs = [&](int row, int col){
-            vis[row][col] = true;
-            for(auto t : dirs){
-                int new_r = row + t[0];
-                int new_c  = col + t[1];
-
-                if(new_r >= 0 && new_r < rows && new_c >= 0 && new_c <cols && !vis[new_r][new_c] && grid[new_r][new_c] == '1'){
-                    dfs(new_r,new_c);
-                }
-            }
-        }; 
 
         int count = 0;
 
-        for(int i =0; i <rows ; i++){
-            for(int j = 0 ; j <cols ; j++){
+        bool vis[rows][cols];
+        memset(vis, false,sizeof(vis));
+
+        vector<vector<int>> dirs = {{1,0} ,{0,1} ,{-1,0},{0,-1}};
+
+        function<void(int,int)> bfs = [&](int row, int col){
+            if(row < 0 || col < 0 || row >= rows || col >=cols || grid[row][col] == '0' || vis[row][col]) return;
+
+            vis[row][col] = true;
+
+            for(auto t : dirs){
+                int new_r = row + t[0];
+                int new_c = col + t[1]; 
+
+                bfs(new_r,new_c);
+            }
+        };
+
+        for(int i = 0; i<rows;i++){
+            for(int j = 0; j<cols; j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
                     count++;
-                    dfs(i,j);
+                    bfs(i,j);
                 }
             }
         }
+
         return count;
     }
 };
