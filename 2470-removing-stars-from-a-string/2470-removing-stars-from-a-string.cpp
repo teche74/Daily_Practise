d8a4a475bb3eb12c30c063bce065326d4e85d7fa
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-04 21:29:24 +0530

    Time: 86 ms (55.53%), Space: 27.5 MB (52.7%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -14,14 +14,24 @@ public:
             }
         }
 
-        string res = "";
+        // string res = "";
 
-        while(!st.empty()){
-            res.push_back(st.top());
+        // while(!st.empty()){
+        //     res.push_back(st.top());
+        //     st.pop();
+        // }
+
+        // reverse(res.begin(),res.end());
+        // return res;
+
+        int size = st.size();
+        string res(size,'.');
+
+        for(int i = size-1 ; i >= 0; i--){
+            res[i] = st.top();
             st.pop();
         }
 
-        reverse(res.begin(),res.end());
         return res;
     }
 };
