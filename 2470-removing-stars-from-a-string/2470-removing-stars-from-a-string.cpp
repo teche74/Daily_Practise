1a0895da440663d0582bbb773f2f553b525d518d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-04 21:27:45 +0530

    Time: 89 ms (40.11%), Space: 28.6 MB (18.77%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+
+        for(char ch : s){
+            if(ch == '*'){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(ch);
+            }
+        }
+
+        string res = "";
+
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
