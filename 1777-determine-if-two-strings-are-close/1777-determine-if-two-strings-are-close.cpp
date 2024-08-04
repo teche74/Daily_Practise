751af632e33e6c3b91f498b21f3bb818ffe75a38
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-04 21:19:25 +0530

    Time: 68 ms (63.32%), Space: 22.2 MB (67.09%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -1,63 +1,33 @@
 class Solution {
 public:
-    bool closeStrings(string str, string ptr) {
-        int n = str.size(), m = ptr.size();
+    bool closeStrings(string word1, string word2) {
+
+        int n = word1.size(), m = word2.size();
 
         if(n != m) return false;
 
-        int cnt1[26] , cnt2[26];
 
-        memset(cnt1, 0 , sizeof(cnt1));
-        memset(cnt2, 0 ,sizeof(cnt2));
+        int freq_w1[26] , freq_w2[26];
 
-        for(int i =0; i < n ; i++){
-            cnt1[str[i] - 'a']++;
-            cnt2[ptr[i] - 'a']++;
+        memset(freq_w1,0,sizeof(freq_w1));
+        memset(freq_w2,0,sizeof(freq_w2));
+
+        for(int i = 0 ;i < n ; i++){
+            freq_w1[word1[i] - 'a']++;
+            freq_w2[word2[i] - 'a']++;
         }
 
-        // cout<<"str1 : ";
-
-        // for(int i =0; i < 26 ; i++){
-        //     cout<<cnt1[i]<<" ";
-        // }
-        // cout<<endl;
-
-        // cout<<"str2 : ";
-
-        // for(int i =0; i < 26 ; i++){
-        //     cout<<cnt2[i]<<" ";
-        // }
-        // cout<<endl;
-
-        for(int i =0; i < 26; i++){
-            if((cnt1[i] == 0 && cnt2[i] != 0) || (cnt2[i] == 0 && cnt1[i] != 0)) return false;
+        for(int i = 0 ;i  < 26 ; i++){
+            if(freq_w1[i] != 0 && freq_w2[i] == 0 || freq_w1[i] == 0 && freq_w2[i] != 0) return false;  
         }
 
+        sort(freq_w1 , freq_w1 + 26);
+        sort(freq_w2 , freq_w2 + 26);
 
-        sort(cnt1 , cnt1 + 26);
-        sort(cnt2 , cnt2 + 26);
-        
-        // cout<<"After sorting >>>"<<endl;
-
-        // cout<<"str1 : ";
-
-        // for(int i =0; i < 26 ; i++){
-        //     cout<<cnt1[i]<<" ";
-        // }
-        // cout<<endl;
-
-        // cout<<"str2 : ";
-
-        // for(int i =0; i < 26 ; i++){
-        //     cout<<cnt2[i]<<" ";
-        // }
-        // cout<<endl;
-
-        for(int i =0 ; i < 26 ; i++){
-            if(cnt1[i] != cnt2[i]){
-                return false;
-            }
+        for(int i = 0 ;i < 26; i++){
+            if(freq_w1[i] != freq_w2[i]) return false;
         }
+
         return true;
     }
 };
