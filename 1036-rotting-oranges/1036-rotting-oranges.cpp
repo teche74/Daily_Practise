da9338832923e72823f3693d0d64533700acaa99
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-04 16:59:49 +0530

    Time: 3 ms (84.13%), Space: 17.6 MB (5.51%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,60 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int rows = grid.size() , cols = grid[0].size();
+        int rows = grid.size() , cols = grid[0].size(), fresh = 0;
 
-        queue<pair<pair<int,int>,int>> q;
-        int res = 0;
+        queue<pair<pair<int,int>,int>>q;
+        bool vis[rows][cols];
+        memset(vis,false,sizeof(vis));
+        
 
-
-        vector<vector<bool>>vis(rows, vector<bool>(cols, false));
-
-        for(int i = 0; i <rows ; i++){
-            for(int j =0; j < cols; j++){
+        for(int i = 0 ; i <rows ; i++){
+            for(int j = 0; j < cols ; j++){
                 if(grid[i][j] == 2){
                     q.push({{i,j},0});
-                    vis[i][j] = 1;
                 }
-                else{
-                    vis[i][j] = 0;
+                else if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
 
-        vector<vector<int>>dirs = {{0,1}, {1,0} , {-1,0} , {0,-1}};
+        int res = 0;
+
+        vector<vector<int>>dirs = {{0,1},{-1,0} ,{1,0} ,{0,-1}};
 
         while(!q.empty()){
-            int curr_row = q.front().first.first;
-            int curr_col = q.front().first.second;
-            int time = q.front().second;
-
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time =q.front().second;
             q.pop();
 
             res = max(res,time);
 
             for(auto t : dirs){
-                int new_r = curr_row + t[0];
-                int new_c = curr_col + t[1];
+                int new_r = row + t[0];
+                int new_c = col + t[1];
 
-
-                if(new_r >= 0 && new_r < rows  && new_c >=0 && new_c < cols && !vis[new_r][new_c] && grid[new_r][new_c] == 1){
-                    q.push({{new_r,new_c},time+1});
+                if(new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols && grid[new_r][new_c] == 1 && !vis[new_r][new_c] ){
+                    fresh--;
                     vis[new_r][new_c] = true;
+                    q.push({{new_r,new_c},time+1});
                 }
-            }
+            } 
         }
 
+        if(fresh != 0 ) return -1;
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols ;j++){
-                if(grid[i][j]==1 && !vis[i][j]){
-                    return -1;
-                }
-            }
-        }
-        
         return res;
-
 
     }
 };
