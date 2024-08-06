6a5f3cafcb986a5c4dd79b0632dea78181b4bd01
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-06 09:28:56 +0530

    Time: 8 ms (89.2%), Space: 24.1 MB (32.52%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,48 +11,20 @@
  */
 class Solution {
 public:
-    void solve(TreeNode * root , int & prev , int  & res){
+    void inorder(TreeNode *root, int & prev, int & res){
         if(root){
-            solve(root->left , prev,res);
-            if(prev != -1 ) res = min(res, root->val - prev );
+            inorder(root->left ,prev,res);
+            if(prev != -1){
+                res = min(res,root->val - prev);
+            }
             prev = root->val;
-            solve(root->right,prev,res);
+            inorder(root->right,prev,res);
         }
     }
     int getMinimumDifference(TreeNode* root) {
-        int prev = -1 , res = INT_MAX;
-        solve(root,prev,res);
-
+        int prev = -1;
+        int res = INT_MAX;
+        inorder(root,prev,res);
         return res;
-        
     }
 };
-
-
-// sara array chahiye hi ni tha bas previous to chahiye tha /.
-
-
-// class Solution {
-// public:
-//     void solve(TreeNode * root , vector<int> & ele)
-//     {
-//         if(root){
-//             solve(root->left,ele);
-//             ele.emplace_back(root->val);
-//             solve(root->right,ele);
-//         }
-//     }
-//     int getMinimumDifference(TreeNode* root) {
-//         vector<int>ele;
-
-//         solve(root, ele);
-
-//         int diff = INT_MAX;
-
-//         for(int i =1; i < ele.size() ; i++){
-//             diff = min(diff, ele[i] - ele[i-1]);
-//         }
-        
-//         return diff;
-//     }
-// };
