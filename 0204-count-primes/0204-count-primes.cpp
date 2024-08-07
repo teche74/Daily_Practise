df573d1d144211d9c18d65f79bf8d93c9b7af500
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-07 19:12:48 +0530

    Time: 209 ms (67.52%), Space: 132.1 MB (8.67%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int>check(n+1,0);
+        vector<int>check(n+1,1);
 
-        for(int i = 1; i*i <= n ; i++){
-            for(double j = i*i ; j <= n ; j+=i){
-                check[j]++;
+        for(int i = 2; i*i < n ; i++){
+            if(check[i] == 1){
+                for(double j = i*i ; j <= n ; j+=i){
+                    check[j]++;
+                }
             }
         }
 
