3e9657393e6476d3caad267fb17cc2d84a5fa3b8
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-07 18:39:55 +0530

    Time: 133 ms (5.08%), Space: 26 MB (60.72%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         map<string,pair<int,bool>>map;
         int low = 0, high = 0, size = s.size();
         vector<string> res;
 
         while(high < size){
             
-            while(high - low + 1 >= 11){
-                low++;
-            }
-
-            if(high - low +1 == 10){
+            while(high - low + 1 >= 10){
                 string str = s.substr(low, high - low+1);
                 map[str].first++;
 
@@ -19,7 +17,9 @@ public:
                     map[str].second = true;
                     res.emplace_back(str);
                 }
+                low++;
             }
+
             high++;
         } 
         return res;
