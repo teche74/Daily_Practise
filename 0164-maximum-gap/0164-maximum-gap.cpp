501b5a793544453070b6247f9bc6c987b9d49748
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-07 12:22:49 +0530

    Time: 126 ms (80.21%), Space: 92.5 MB (14.72%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int size = nums.size();
 
-        int res = 0 ;
+        if(size < 2 ) return 0;
 
-        for(int i =1 ; i < nums.size() ; i++){
-            res = max(res, nums[i] - nums[i-1]);
+        int maxi = *max_element(nums.begin(),nums.end());
+        int mini = *min_element(nums.begin(), nums.end());
+
+
+        int gap = max(1,(maxi - mini)/(size-1));
+        int buckets  = (maxi - mini)/gap + 1 ;
+
+        vector<int>max_e(buckets,INT_MIN);
+        vector<int>min_e(buckets,INT_MAX);
+
+        for(int num : nums){
+            int id = (num - mini)/gap;
+            min_e[id] = min(min_e[id],num);
+            max_e[id] = max(max_e[id],num);
         }
 
-        return res;
+
+        int ans = 0, prev = mini;
+        for (int i = 0; i < buckets; ++i) {
+            if (min_e[i] == INT_MAX) continue; 
+            ans = max(ans, min_e[i] - prev);
+            prev = max_e[i];
+        }
+
+        return ans;
+
     }
 };
