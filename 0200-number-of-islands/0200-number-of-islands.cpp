500a5042950bd39e4a36b0aa5e3875eb4d26cd12
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-07 18:58:01 +0530

    Time: 116 ms (5.06%), Space: 31.1 MB (6.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,31 +3,32 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size() , cols = grid[0].size();
 
-        int count = 0;
+        vector<vector<bool>>vis(rows,vector<bool>(cols,false));
 
-        bool vis[rows][cols];
-        memset(vis, false,sizeof(vis));
+        vector<vector<int>> dirs = {{0,1}, {1,0} ,{-1,0} , {0,-1}};
 
-        vector<vector<int>> dirs = {{1,0} ,{0,1} ,{-1,0},{0,-1}};
-
-        function<void(int,int)> bfs = [&](int row, int col){
-            if(row < 0 || col < 0 || row >= rows || col >=cols || grid[row][col] == '0' || vis[row][col]) return;
+        function<void(int,int)> solve = [&](int row, int col){
+            if(row >= rows || col >= cols || col < 0 || row < 0 || vis[row][col] || grid[row][col] != '1'){
+                return;
+            }
 
             vis[row][col] = true;
 
             for(auto t : dirs){
                 int new_r = row + t[0];
-                int new_c = col + t[1]; 
+                int new_c = col + t[1];
 
-                bfs(new_r,new_c);
+                solve(new_r , new_c);
             }
         };
 
-        for(int i = 0; i<rows;i++){
-            for(int j = 0; j<cols; j++){
+        int count  = 0;
+
+        for(int i = 0; i < rows ; i++){
+            for(int j =0; j < cols ; j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
                     count++;
-                    bfs(i,j);
+                    solve(i,j);
                 }
             }
         }
