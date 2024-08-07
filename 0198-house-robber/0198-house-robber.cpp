83d49a53c1059b4d9398061572ce5dd2f0038e84
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-07 18:47:10 +0530

    Time: 0 ms (100%), Space: 9.3 MB (44.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     int dp[101];
     int solve(int index, vector<int> & nums){
-        if(index >= nums.size() ){
-            return 0;
-        }
+        if(index >= nums.size()) return 0;
 
         if(dp[index] != -1) return dp[index];
 
-        int take = 0 , not_take = 0;
-
-        take = nums[index] + solve(index+2, nums);
-
-        not_take = solve(index+1,nums);
+        int take = nums[index] + solve(index+2,nums);
+        int not_take = solve(index+1,nums);
 
         return dp[index] = max(take, not_take);
     }
     int rob(vector<int>& nums) {
-        int size = nums.size();
-
-        memset(dp, -1, sizeof(dp));
-
-        return solve(0, nums);
+        memset(dp,-1,sizeof(dp));
+        return solve(0 , nums);
     }
 };
