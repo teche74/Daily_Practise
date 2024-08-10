d08d955b4ef0810c4ed5c0cf6d825162ae34f6bc
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-10 17:33:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        int low = 0 , high = 0 , size = s.size();
-        int uni = 0, res= 0 ;
-        int freq[26];
-        memset(freq,0,sizeof(freq));
+        int low = 0 , high = 0 , size = s.size() , res = 0 ;
+        int freq[26] = {0};
 
-        while(high < size){
+        while( high  < size){
             freq[s[high] - 'a']++;
-            uni  += (freq[s[high] - 'a'] == 1);
 
-            while(uni > 3 || high - low+1 > 3){
+            while(freq[s[high] - 'a'] > 1){
                 freq[s[low] - 'a']--;
-                uni -= (freq[s[low]- 'a'] == 0);
                 low++;
             }
 
-            if(uni == 3) res++;
+            res += (high - low + 1 == 3);
             high++;
-        }
+        } 
+
         return res;
     }
 };
