bca3560b5e73565dfe27aa5f34371ee3a03c72e1
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-10 17:59:26 +0530

    Time: 5 ms (31.94%), Space: 10.5 MB (9.1%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int size = code.size();
-        vector<int>res(size,0);
 
-        if(k > 0 )
-        {
-            for(int i =0 ;i < size ;i++){
-                int sum = 0 ;
-                for(int j = 1 ; j <= k ; j++){
-                    sum += code[(i+j) % size];
+        vector<int>res;
+        int low , high,sum;
+        if(k > 0){
+            low = 1 , high = 1 , sum = 0;
+
+            while(res.size() < size){
+                sum += code[high % size];
+
+                while(high - low +1  > k){
+                    sum -= code[low % size];
+                    low++;
                 }
-                res[i] = sum;
+                if(high - low + 1 == k || k == 0) res.emplace_back(sum);
+                high++;
             }
         }
         else{
-            for(int i =0 ;i < size ;i++){
-                int sum = 0 ;
-                for(int j = 1 ; j <= -k ; j++){
-                    sum += code[(i - j + size) % size];
+            k*=-1;
+            k %= size;
+
+            low = size-k , high = size-k, sum = 0;
+
+            while(res.size() < size){
+                sum += code[high % size];
+
+                while(high - low + 1  > k){
+                    sum -= code[low % size];
+                    low++;
                 }
-                res[i] = sum;
+                if(high - low + 1 == k) res.emplace_back(sum);
+                high++;
             }
         }
         return res;
