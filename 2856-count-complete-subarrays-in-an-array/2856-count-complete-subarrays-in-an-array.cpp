18120c836080ac184817b606ee7645df52158dbd
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-11 18:10:13 +0530

    Time: 23 ms (92.84%), Space: 45.1 MB (48.4%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int> & nums, int k){
-        int low= 0 , high = 0 ,size = nums.size(), count = 0, res= 0;
+    int solve(vector<int> & nums, int k){
+        int low = 0 , high = 0  , size = nums.size(), res = 0;
         unordered_map<int,int>map;
 
         while(high < size){
             map[nums[high]]++;
-            count += (map[nums[high]] == 1);
 
-            while(count > k){
+            while(map.size() > k){
                 map[nums[low]]--;
-                count-=(map[nums[low]] == 0);
+                if(map[nums[low]] == 0){
+                    map.erase(nums[low]);
+                }
                 low++;
             }
-            res += high-low+1;
+            res  += (high - low+1);
             high++;
         }
-
         return res;
     }
     int countCompleteSubarrays(vector<int>& nums) {
+        int size = nums.size();
+
         unordered_set<int>st(nums.begin(), nums.end());
-        int k = st.size();
-        return helper(nums,k) - helper(nums, k-1);
+
+        return solve(nums, st.size()) - solve(nums, st.size()-1);
     }
 };
