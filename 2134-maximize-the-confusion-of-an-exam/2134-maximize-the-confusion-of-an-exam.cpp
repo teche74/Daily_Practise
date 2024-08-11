83fe8d18eb8d172f6913fb37cb2edc9fdf20630d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-11 17:41:48 +0530

    Time: 15 ms (97.88%), Space: 11.6 MB (48.48%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int helper( const string & s , int k , char ch){
-        int low =0  , high = 0 , size = s.size() , res = 0 ;
-        int opers = 0;
+    int solve(string & str , char val , int miss){
+        int low = 0 , high = 0 , size = str.size() , res = 0;
 
+        while(high < size){
+            miss-=(str[high] != val);
 
-        while( high < size){
-            opers += (s[high] != ch);
-
-            while(opers > k){
-                opers -= (s[low] != ch);
+            while(miss < 0){
+                miss+=(str[low] != val);
                 low++;
-            }   
+            }
 
-            res = max(res,high - low+1 );
+            res = max(res, high - low+1);
             high++;
         }
         return res;
     }
-    int maxConsecutiveAnswers(string arr, int k) {
-        return max(helper(arr,k,'T'), helper(arr,k,'F')); 
+    int maxConsecutiveAnswers(string str, int k) {
+        return max(solve(str,'T', k) , solve(str, 'F', k));
+
     }
 };
