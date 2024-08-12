336a15ee149988e5c305ca1080a4989d44b15a3f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-12 07:46:36 +0530

    Time: 23 ms (81.01%), Space: 24.8 MB (17.04%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,25 +1,26 @@
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>> heap;
+    priority_queue<int,vector<int>,greater<int>>pq;
     int size = 0;
+
     KthLargest(int k, vector<int>& nums) {
         size = k;
 
-        for(auto t : nums){
-            heap.push(t);
-
-            if(heap.size() > size){
-                heap.pop();
+        for(int x : nums){
+            pq.push(x);
+            if(pq.size() > size){
+                pq.pop();
             }
-        }
+        }    
     }
     
     int add(int val) {
-        heap.push(val);
-        if(heap.size() > size){
-            heap.pop();
+        pq.push(val);
+        if(pq.size() > size){
+            pq.pop();
         }
-        return heap.top();
+
+        return pq.top();
     }
 };
 
