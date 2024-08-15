b8b6fe1fe1bb65c29d8bdbab6fc72fb544004e31
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-15 09:59:03 +0530

    Time: 3 ms (62.65%), Space: 18 MB (5.39%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,24 +11,24 @@
  */
 class Solution {
 public:
-    void solve(TreeNode * root,  string temp , vector<string> & res){
+    void inorder(TreeNode* root, string temp , vector<string> & res){
         if(root){
-            temp +=to_string(root->val);
+            temp+=to_string(root->val);
             if(!root->left && !root->right){
                 res.emplace_back(temp);
             }
-            else{
-                temp+="->";
-            }
-            solve(root->left,temp,res);
-            solve(root->right,temp,res);
+            temp+="->";
+            inorder(root->left, temp, res);
+            inorder(root->right, temp, res);
+            while(temp.back() != '>') temp.pop_back();
         }
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string>res;
-        string temp = "";
+        string temp= "";
 
-        solve(root,temp,res);
+        inorder(root,temp,res);
+
         return res;
     }
 };
