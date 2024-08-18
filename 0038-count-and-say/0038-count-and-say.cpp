63c468c80b532612f056a727302a809bad720cc2
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 21:50:38 +0530

    Time: 2 ms (82.98%), Space: 10.4 MB (37.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-
     string countAndSay(int n) {
         if(n == 1){
-            return "1";
+            return  "1";
         }
 
-        string prev = countAndSay(n-1);
+        string str = countAndSay(n-1);
+        char prev = str[0]; 
+        int count  = 0;
         string res = "";
 
-        int count = 0;
-        char curr = prev[0];
-
-        for(char ch : prev){
-            if(ch == curr){
+        for(int i = 0 ; i < str.size() ; i++){
+            if(str[i] == prev){
                 count++;
             }
             else{
-                res += to_string(count) + curr;
-                curr =ch;
+                res += to_string(count) + prev;
+                prev =str[i];
                 count =1;
             }
         }
-
-        res += to_string(count) + curr;
+        res += to_string(count) + prev;
         return res;
     }
 };
