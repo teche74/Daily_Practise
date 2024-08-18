14a9afcd8859514a485065b41f49df4c72efa32c
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 10:51:01 +0530

    Time: 62 ms (65.52%), Space: 27.4 MB (69.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int size = nums.size();
-        sort(nums.begin(),nums.end());
-
-
-        // brute;
-        // set<vector<int>>res;
-        // for(int i = 0 ; i <size; i++){
-        //     for(int j = i+1;  j < size ; j++ ){
-        //         for( int k = j+1; k  < size ; k ++){
-        //             if(nums[i] + nums[j] + nums[k] == 0){
-        //                 res.insert({nums[i], nums[j], nums[k]});
-        //             }
-        //         }
-        //     }
-        // }
-        // return {res.begin(),res.end()};
-
-
-        // optimized
+        int size =  nums.size();
+        sort(nums.begin() , nums.end());
         vector<vector<int>>res;
-        for(int i = 0 ;i < size; i ++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+        for(int i = 0 ; i < size ; i++){
+            if(i > 0  && nums[i] == nums[i-1]) continue;
 
             int j = i+1;
             int k = size-1;
 
-            while(j < k){
+            while(j < k ){
                 int sum = nums[i] + nums[j] + nums[k];
 
                 if(sum == 0){
-                    res.push_back({nums[i], nums[j] , nums[k]});
+                    res.emplace_back(vector<int>{nums[i] , nums[j] , nums[k]});
                     j++;
                     k--;
 
-                    while(j <k && nums[j] == nums[j-1]) j++;
-                    while(j <k && nums[k] == nums[k+1]) k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }
                 else if(sum > 0){
                     k--;
