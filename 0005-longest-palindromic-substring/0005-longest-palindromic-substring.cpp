f9014b393cf0b93465e39a423e3a2a23ba31a0f3
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 10:29:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -28,8 +28,60 @@ public:
 
         return str.substr(ind,len);
     }
+
+    string manchers(const string  & str ){
+        string s = "$";
+
+        for(char ch : str){
+            s.push_back('|');
+            s.push_back(ch);
+        }
+
+        s.push_back('|');
+        s.push_back('@');
+
+        int size = s.size();
+        int lps[size];
+        memset(lps, 0 ,sizeof(lps));
+
+        int centre = 0 , right =  0;
+
+        for(int i = 1; i  < size -1 ; i++){
+            int mirror  = 2 * centre - i;
+
+            if(i < right){
+                lps[i] = max( right - i, lps[mirror]);
+            }
+
+            while(s[i + lps[i] + 1] == s[i - lps[i] - 1]){
+                lps[i]++;
+            }
+
+            if(i + lps[i]  > right){
+                centre =  i;
+                right = i + lps[i];
+            }
+        }
+
+        int max_len = 0 , max_centre = 0;
+
+        for(int i  =1; i  < size ; i++){
+            if(lps[i] >  max_len){
+                max_len = lps[i];
+                max_centre = i;
+            }
+        } 
+
+
+        return str.substr((max_centre - max_len)/2 , max_len); 
+    }
+
     string longestPalindrome(string s) {
         // centre Expand apporach.
-        return centreexpand(s);
+        // return centreexpand(s);
+
+
+        // manchers algorith
+        return manchers(s);
     }
 };
