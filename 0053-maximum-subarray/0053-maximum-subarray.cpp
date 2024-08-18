84ef9d2ddacca519ca1fa46f5325f50db63fe27f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 22:28:10 +0530

    Time: 88 ms (10.92%), Space: 70.7 MB (5.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // kadane
-        // int sum = 0 , max_sum = INT_MIN;
-
-        // for(int x :  nums){
-        //     sum+=x;
-
-        //     if(sum < x){
-        //         sum = x;
-        //     }
-
-        //     max_sum = max(max_sum , sum);
-        // } 
-
-        // return max_sum;
-
-
-        // prefix map
-
         int size = nums.size();
 
-        int dp[size+1];
-        
-        memset(dp, 0 ,sizeof(dp));
+        int sum = 0 , max_sum = INT_MIN ;
 
-        dp[0] = nums[0];
+        for(int x : nums){
+            sum+=x;
 
-        for(int i = 1; i < size ; i++){
-            dp[i] = max( nums[i] , dp[i-1] + nums[i] );
+            if(sum < x){
+                sum = x;
+            }
+
+            max_sum = max(sum , max_sum);
         }
 
-        return *max_element(dp, dp + size);
-        
+        return max_sum;
     }
 };
