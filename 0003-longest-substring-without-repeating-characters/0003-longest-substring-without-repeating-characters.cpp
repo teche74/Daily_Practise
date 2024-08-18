2945df2f8d56983486cf5b10731c84ab20bb07d1
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 10:11:19 +0530

    Time: 19 ms (39.94%), Space: 11.8 MB (58.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,19 +3,39 @@ public:
     int lengthOfLongestSubstring(string s) {
         // approach 1 : set;
 
-        int low = 0 , high = 0 , size = s.size() , res = 0;
-        unordered_set<char>st;
+        // int low = 0 , high = 0 , size = s.size() , res = 0;
+        // unordered_set<char>st;
 
 
-        while(high  < size){
-            if(st.find(s[high]) == st.end()){
-                st.insert(s[high++]);
-                res = max(res, high - low);
+        // while(high  < size){
+        //     if(st.find(s[high]) == st.end()){
+        //         st.insert(s[high++]);
+        //         res = max(res, high - low);
+        //     }
+        //     else{
+        //         st.erase(s[low++]);
+        //     }
+        // }
+        // return res;
+
+
+        // unordereded map  approach 2
+
+        int low = 0 , high = 0 , size = s.size() , res = 0 ;
+        unordered_map<char,int>map;
+
+        while(high < size){
+            map[s[high]]++;
+
+            while(map[s[high]] > 1){
+                map[s[low]]--;
+                low++;
             }
-            else{
-                st.erase(s[low++]);
-            }
-        }
+
+            res = max(res, high - low+1);
+            high++;
+        } 
+
         return res;
     }
 };
