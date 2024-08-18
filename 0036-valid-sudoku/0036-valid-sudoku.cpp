277945a9d93ef6efa0093ac37631f1ee78cb967b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 21:26:27 +0530

    Time: 7 ms (98.52%), Space: 24 MB (54.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& arr) {
-        int size = 9;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int size = board.size();
 
-        int row[size][size], col[size][size], grid[size][size];
-        memset(row,0, sizeof(row));
-        memset(row,0, sizeof(col));
-        memset(row,0, sizeof(grid));
+        vector<vector<int>>row_check(size,vector<int>(size,0));
+        vector<vector<int>>col_check(size,vector<int>(size,0));
+        vector<vector<int>>grid_check(size,vector<int>(size,0));
 
-        for(int i = 0 ; i < size; i ++){
-            for(int j = 0 ;j <size; j++){
-                if(arr[i][j] != '.'){
-                    int val = arr[i][j] - '0';
 
-                    int k = (i/3) * 3 + (j/3);
+        for(int i = 0 ;i < size ; i++){
+            for(int j = 0; j < size ; j++){
+                if(board[i][j] != '.'){
+                    int val = board[i][j] - '0';
 
-                    if(row[val-1][j]++ || col[i][val-1]++ || grid[val-1][k]++){
-                        return false;
-                    }
+                    int k  = (i/3) * 3 + (j/3);
+
+                    if(row_check[i][val-1]++ || col_check[val-1][j]++ || grid_check[val-1][k]++ ) return false;
                 }
             }
         }
+
         return true;
+
     }
 };
