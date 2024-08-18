3dee621d1152004894f4e0ff34a2a3b504bc3da3
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 10:34:08 +0530

    Time: 28 ms (18.59%), Space: 17 MB (17.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,59 +3,29 @@ public:
     string convert(string s, int numRows) {
         int size = s.size();
 
-        if(size < numRows ) return s;
-        if(numRows == 1) return s;
+        if(numRows  > size ||  numRows == 1) return s;
 
-        //  brute 
-        // vector<char>st[numRows];
-        // int row = 0 , dir = -1; 
+        int row = 0 , dir = -1;
 
-        // for(char ch : s){
-        //     st[row].push_back(ch);
-
-        //     if(row == 0 || row == numRows-1){
-        //         dir*=-1;
-        //     }
-
-        //     row+=dir;
-        // }
-
-        // string res = "";
-
-        // for(auto t : st){
-        //     for(char ch : t){
-        //         res.push_back(ch);
-        //     }
-        // }
-
-        // return res;
-
-
-
-
-
-        // optimize
-
-        vector<string>st(numRows,"");
-        int row = 0 , dir = -1; 
+        vector<char>st[numRows];
 
         for(char ch : s){
             st[row].push_back(ch);
 
-            if(row == 0 || row == numRows-1){
-                dir*=-1;
+            if(row == numRows -1 || row == 0){
+                dir *= -1;
             }
 
-            row+=dir;
-        }
+            row +=dir;
+        } 
 
         string res = "";
 
-        for(auto t : st){
-            res+=t;
-        }
+        for(auto ch  :st){
+            for(auto t : ch)
+                res.push_back(t);
+        } 
 
         return res;
-
     }
 };
