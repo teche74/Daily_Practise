e619fbcb32b3c01e648840475c227c1e13c70fc4
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 10:13:39 +0530

    Time: 4 ms (90.44%), Space: 10.8 MB (79.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -21,21 +21,36 @@ public:
 
         // unordereded map  approach 2
 
-        int low = 0 , high = 0 , size = s.size() , res = 0 ;
-        unordered_map<char,int>map;
+        // int low = 0 , high = 0 , size = s.size() , res = 0 ;
+        // unordered_map<char,int>map;
 
-        while(high < size){
-            map[s[high]]++;
+        // while(high < size){
+        //     map[s[high]]++;
 
-            while(map[s[high]] > 1){
-                map[s[low]]--;
-                low++;
-            }
+        //     while(map[s[high]] > 1){
+        //         map[s[low]]--;
+        //         low++;
+        //     }
 
-            res = max(res, high - low+1);
-            high++;
-        } 
+        //     res = max(res, high - low+1);
+        //     high++;
+        // } 
 
+        // return res;
+
+
+        // quick access (last seen) 3rd apporach
+
+        int map[256];
+
+        memset(map,-1, sizeof(map));
+        int low = 0, size = s.size(), res=  0;
+
+        for(int high = 0 ;high < size ;high++){
+            low = max(low, map[s[high]]+1);
+            res= max(res, high - low+1);
+            map[s[high]] = high;
+        }
         return res;
     }
 };
