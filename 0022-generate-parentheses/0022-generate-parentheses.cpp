24fb6ee844ccf23673a05cb354ca6b91b70aa6c4
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-18 11:12:51 +0530

    Time: 4 ms (44.44%), Space: 16.1 MB (19.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string>res;
-        string temp = "";
+    void solve(int open , int close ,  string temp  , vector<string> & res){
+        if(open == 0 && close == 0){
+            res.emplace_back(temp);
+            return ;
+        }
 
-        function<void(int,int)> solve = [&](int open, int close)
-        {
-            if(open == 0 && close == 0){
-                res.emplace_back(temp);
-                return;
-            }
+        if(open > 0){
+            temp.push_back('(');
+            solve(open-1, close, temp,res);
+            temp.pop_back();
+        }
 
-            if(open > 0){
-                temp.push_back('(');
-                solve(open-1,close);
+        if(close > 0){
+            if(close > open){
+                temp.push_back(')');
+                solve(open, close -1 , temp,res);
                 temp.pop_back();
             }
-            
-            if(close > 0){
-                if(close > open){
-                    temp.push_back(')');
-                    solve(open,close-1);
-                    temp.pop_back();
-                }
-            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        string temp =  "";
 
-        };
-
-        solve(n,n);
-
+        solve(n,n, temp,res);
         return res;
     }
 };
