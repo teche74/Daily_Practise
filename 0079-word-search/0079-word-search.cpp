7b5d872f1229fc4f1627fb11d3aa9d88047fc6f2
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-19 09:42:41 +0530

    Time: 797 ms (44.19%), Space: 11 MB (61.07%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int rows = board.size() , cols = board[0].size();
+        int rows = board.size()  , cols = board[0].size();
 
-        if (rows * cols < word.size()) return false;
+        vector<vector<int>>dirs = {{0,1},  {1,0} , {-1,0} , {0,-1}};
 
-        unordered_map<char, int> wordCount, boardCount;
-        for (char c : word) wordCount[c]++;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                boardCount[board[i][j]]++;
-            }
-        }
-        
-        
-        for (auto& wc : wordCount) {
-            if (boardCount[wc.first] < wc.second) return false;
-        }
+        function<bool(int,int,int)> solve = [&](int index, int row, int col){
+            if(index >= word.size()) return true;
+            if(row >= rows || col  >= cols || row < 0 || col  < 0 || board[row][col] == '.' || board[row][col] != word[index]) return false;
 
-        vector<vector<int>>dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-
-        function<bool(int,int,int)> dfs = [&](int row, int col, int len){
-            if(len == word.size()) return true;
-
-            if(row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != word[len]) 
-                return false;
-
-            char ch = board[row][col];
+            char ch  = board[row][col];
             board[row][col] = '.';
-            len++;
+            index++;
 
-            for(auto t : dirs){
-                int new_r = t[0] + row;
-                int new_c = t[1] + col;
+            if(solve(index , row+1,col) || solve(index , row,col+1) || solve(index , row-1, col) || solve(index, row, col-1)) return true;
 
-                if(dfs(new_r, new_c, len)) {
-                    board[row][col] = ch;  
-                    return true;
-                }
-            }
             board[row][col] = ch;
             return false;
         };
 
-        for(int i =0; i <rows;  i++){
-            for(int j =0; j< cols ; j++){
-                if(board[i][j] == word[0] && dfs(i,j,0)){
+        for(int i = 0; i  < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(board[i][j] == word[0] && solve(0,i,j)){
                     return true;
                 }
             }
-        } 
+        }
+
         return false;
     }
 };
