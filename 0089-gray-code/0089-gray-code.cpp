3d08fcaed4fdb755a7f06deed455aa57e3bc426d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-19 10:08:19 +0530

    Time: 4 ms (71.97%), Space: 14.2 MB (18.1%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int>res;
-
-        int size = 1 << n;
-
-        for(int i = 0 ;i < size ; i++){
-            res.emplace_back(i ^ (i >> 1));
+        if (n == 0) {
+            return {0};
         }
-        
+
+        vector<int> prev = grayCode(n - 1);
+        vector<int> res = prev;
+
+        int lead = 1 << (n - 1);  
+
+        for (int i = prev.size() - 1; i >= 0; i--) {
+            res.push_back(lead | prev[i]);
+        }
+
         return res;
     }
 };
