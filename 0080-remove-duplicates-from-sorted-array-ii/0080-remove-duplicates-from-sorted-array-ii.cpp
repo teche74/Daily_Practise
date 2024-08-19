ec869aa52f121c2ad3529c98272a6ccdb72bb23a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-19 09:50:55 +0530

    Time: 0 ms (100%), Space: 13.6 MB (49.6%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,17 +3,14 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int size =  nums.size();
 
-        unordered_map<int,int>map;
-        int cnt{0}, ind =0;
+        int index = 0;
 
-        for(int i =0; i <size ; i++){
-            if(map.find(nums[i]) == map.end() || map[nums[i]] < 2){
-                map[nums[i]]++;
-                nums[ind] = nums[i];
-                ind++;
-                cnt++;
+        for(int i  = 0 ;i  < nums.size() ; i++){
+            if(i >= 2 && nums[i] == nums[index -2 ]){
+                continue;
             }
+            else    nums[index++] = nums[i];
         }
-        return cnt;
+        return index;
     }
 };
