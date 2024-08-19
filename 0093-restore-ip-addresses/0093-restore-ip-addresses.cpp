31cb1136529c018f839e59456215c30983e66d2a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-19 11:08:21 +0530

    Time: 5 ms (36.63%), Space: 11.4 MB (26.49%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
-    void solve(int index, const string &s, string temp, vector<string> &res, int count) {
-        if (count == 4) {
-            if (index == s.size()) {
+    void solve(int index, string str,  string temp , vector<string> & res, int stops){
+        if (stops == 0) {
+            if (index == str.size()) {
                 res.push_back(temp);
             }
             return;
         }
 
-        int num = 0;
-        for (int i = index; i < s.size(); i++) {
-            num = num * 10 + (s[i] - '0');
-            if (num <= 255) {
-                if (i > index && s[index] == '0')  
-                    break;
-                string segment = to_string(num);
-                if (count < 3)
-                    segment.push_back('.');
-                solve(i + 1, s, temp + segment, res, count + 1);
-            } else {
+        int num = 0 ;
+
+
+        for(int i = index; i < str.size() ; i++){
+            num *= 10;
+            num +=(str[i] - '0');
+
+            if(num <= 255){
+                if(i > index && str[index] == '0') break;
+                
+                string seg = to_string(num);
+                if(stops > 1)
+                    seg.push_back('.');
+                
+                solve(i+1,str,temp + seg,res,stops-1);
+            }else{
                 break;
             }
         }
-    }
 
+
+
+    }
     vector<string> restoreIpAddresses(string s) {
-        vector<string> res;
-        solve(0, s, "", res, 0);
+        vector<string>res;
+        string temp = "";
+
+        solve( 0 , s, temp,res , 4);
+
         return res;
+
     }
 };
