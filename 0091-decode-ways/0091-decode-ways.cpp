133e464289fc7c3509fadb2812de0ac6698cd192
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-19 10:56:03 +0530

    Time: 4 ms (33.17%), Space: 8.9 MB (41.17%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
-    // int dp[101];
-    // int solve(int index, const string  & s){
-    //     if(index >= s.size()){
-    //         return 1;
-    //     }
-
-    //     if(s[index] == '0') return 0;
-
-    //     if(dp[index] != -1){
-    //         return dp[index];
-    //     }
-
-    //     int single_digit = 0, double_digit = 0;
-
-    //     single_digit = solve(index+1,s);
-
-    //     if(index+1 < s.size()){
-    //         string str = s.substr(index,2);
-    //         if(str >= "1" && str <= "26"){
-    //             double_digit = solve(index+2,s);
-    //         }
-    //     }
-    //     return dp[index] = single_digit + double_digit; 
-    // }
-    int numDecodings(string s) {
-        // memset(dp,-1,sizeof(dp));
-        // return solve(0 , s);
-
-        int size = s.size();
-
-        int dp[size+1];
-        memset(dp,0,sizeof(dp));
-
-        dp[size] = 1;
-
-        for(int i =size-1; i >= 0 ;i--){
-            if(s[i] == '0'){
-                dp[i] = 0;
-            }
-            else{
-                dp[i] = dp[i+1];
-                if ( i+1 < size && (s[i] == '1' || s[i] == '2'  && s[i+1] <= '6' )){
-                    dp[i] += dp[i+2];
-                }
-            }
+    int dp[101];
+    int solve(int index, string & str){
+        if(index >= str.size()){
+            return 1;
         }
 
-        return dp[0];
+        if(str[index] == '0') return 0;
+
+        if(dp[index] != -1) return dp[index];
+
+        int single = solve(index+1,str);
+
+        int doub = 0;
+
+        if(index + 1 < str.size() && str.substr(index,2) <= "26"){
+            doub = solve(index+2 , str);
+        }
+
+        return dp[index] = single + doub;
+    }
+    int numDecodings(string s) {
+        memset(dp , -1 ,sizeof(dp));
+        return solve(0 ,s);
     }
 };
