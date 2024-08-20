a948b5486ba159840c2638d1b02b09c7574cf943
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-20 12:12:53 +0530

    Time: 110 ms (25.49%), Space: 133.7 MB (6.31%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,62 +1,27 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // int res = -1;
+        int size = cost.size();
 
-        // int size = gas.size();
+        vector<int>diff(size, 0);
 
-        // for(int i = 0 ;  i < size; i ++){
-        //     int take =  gas[i];
-        //     // cout<<"Start kiya "<<i<<" se"<<endl;
-
-        //     for(int j = i+1; j <= i + size; j++ ){
-
-        //         if(cost[(j-1) % size] > take){
-        //             // cout<<"not_possible"<<endl;
-        //             break;
-        //         }
-        //         take -=cost[(j-1) % size];
-        //         // cout<<j %size<<" jane ke liye cost lga "<<cost[(j-1) % size]<<endl;
-        //         take+=gas[j % size];
-        //         // cout<<"gas bhra "<<gas[j % size]<<endl;
-
-        //         if(take <= 0){
-        //             // cout<<"not possible"<<endl;
-        //             break;
-        //         }
-
-        //         if(j % size == i){
-        //             // cout<<"Suru kr skten hen index "<<i<<" se"<<endl;
-        //             res =  i;
-        //             break;
-        //         }
-        //     } 
-
-        //     if(res != -1) break;
-        // }
-        // return res;
-
-
-        int size = gas.size();
-
-        vector<int> easy;
-
-        for(int i = 0; i <size ; i++){
-            easy.emplace_back(gas[i] - cost[i]);
+        for(int i = 0 ; i < size ; i++){
+            diff[i] = gas[i] -cost[i];
         }
 
-        int total = 0 , curr = 0, start = 0;
+        int total = 0 , start =  0, curr = 0;
 
-        for(int i = 0; i< size; i++){
-            total += easy[i];
-            curr += easy[i];
+        for(int i = 0; i  < size; i++){
+            curr += diff[i];
+            total += diff[i]; 
 
             if(curr < 0){
+                curr = 0;
                 start = i+1;
-                curr= 0 ;
             }
+
         }
 
-        return total >= 0 ? start : -1; 
+        return total >= 0  ? start : -1;
     }
 };
