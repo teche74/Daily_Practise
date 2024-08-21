9a8488b94d5dbd05b387782fbc8faf7692467ef6
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-21 09:16:50 +0530

    Time: 80 ms (18.42%), Space: 27.8 MB (6.06%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,37 +3,42 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         if(matrix.empty()) return 0;
 
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector<int> heights(cols, 0);
-        int max_side = 0;
+        vector<int>heights(matrix[0].size() , 0);
 
-        for(int i = 0; i < rows; ++i) {
-            for(int j = 0; j < cols; ++j) {
-                heights[j] = (matrix[i][j] == '1') ? heights[j] + 1 : 0;
+        function<int(void)> solve = [&](){
+            stack<int>st;
+            heights.push_back(0);
+            int max_side = 0;
+
+            for(int i = 0 ; i < heights.size() ; i++){
+                while(!st.empty() && heights[i] < heights[st.top()]){
+                    int height = heights[st.top()];
+                    st.pop();
+                    int width = st.empty() ? i : (i - st.top() - 1);
+                    int side =  min(height, width);
+                    max_side = max(max_side , side);
+                }
+                st.push(i);
             }
-            max_side = max(max_side, maximalSquareInHistogram(heights));
-        }
+            return max_side;
+        };
 
-        return max_side * max_side;
-    }
-
-private:
-    int maximalSquareInHistogram(vector<int>& heights) {
-        stack<int> st;
-        heights.push_back(0);  
-        int max_side = 0;
+        int res = 0 ;
         
-        for(int i = 0; i < heights.size(); ++i) {
-            while(!st.empty() && heights[i] < heights[st.top()]) {
-                int height = heights[st.top()];
-                st.pop();
-                int width = st.empty() ? i : (i - st.top() - 1);
-                int side = min(height, width);  
-                max_side = max(max_side, side);
+        for(auto t : matrix){
+            int index = 0;
+            for(int val :  t){
+                if(val == '0'){
+                    heights[index] = 0;
+                }
+                else{
+                    heights[index]+=(val - '0');
+                }
+                index++;
             }
-            st.push(i);
-        }
 
-        return max_side;
+            res = max(res, solve());
+        }
+        return res*res;
     }
 };
