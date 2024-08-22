6708b2c339dbdfbe4958a123f42e89712144bd8f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-22 08:54:15 +0530

    Time: 119 ms (5.02%), Space: 35.9 MB (5.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> res(n);
-        res[0] = 1;
+        if (n <= 0) return 0;
+
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        set<long long> seen;  
+
+        pq.push(1);
+        seen.insert(1);
         
-        int i2 = 0, i3 = 0, i5 = 0;
-        int two_multiple = 2;
-        int three_mulitple = 3;
-        int five_mulitple = 5;
-        
-        for (int i = 1; i < n; i++) {
-            int next = min(two_multiple, min(three_mulitple, five_mulitple));
-            res[i] = next;
-            
-            if (next == two_multiple) {
-                i2++;
-                two_multiple = res[i2] * 2;
+        long long ugly = 1;
+
+        for (long long i = 1; i <= n; i++) {
+            ugly = pq.top();
+            pq.pop();
+
+            if (seen.find(ugly * 2) == seen.end()) {
+                pq.push(ugly * 2);
+                seen.insert(ugly * 2);
             }
-            if (next == three_mulitple) {
-                i3++;
-                three_mulitple = res[i3] * 3;
+            if (seen.find(ugly * 3) == seen.end()) {
+                pq.push(ugly * 3);
+                seen.insert(ugly * 3);
             }
-            if (next == five_mulitple) {
-                i5++;
-                five_mulitple = res[i5] * 5;
+            if (seen.find(ugly * 5) == seen.end()) {
+                pq.push(ugly * 5);
+                seen.insert(ugly * 5);
             }
         }
-        
-        return res[n-1];
+
+        return ugly; 
     }
 };
