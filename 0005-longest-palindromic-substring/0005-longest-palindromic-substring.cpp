0f3590cb6e999e544b0a097f45bbaaf3180b180d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 08:30:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -8,23 +8,71 @@ public:
 
         return high - low - 1;
     }
-    string longestPalindrome(string s) {
-        int size = s.size();
-        int ind = 0 , len = 0 ;
 
-        for(int i = 0 ; i < size; i++){
-            
-            int len1 = solve(s,i,i);
-            int len2 = solve(s,i,i+1);
+    string manchers(const string & str){
+        string ptr = "$";
 
-            int max_len = max(len1,len2);
+        for(char ch : str){
+            ptr.push_back('|');
+            ptr.push_back(ch);
+        }
+        ptr.push_back('|');
+        ptr.push_back('@');
 
-            if(max_len > len){
-                len = max_len;
-                ind = i - (max_len -1)/2;
+        int size = ptr.size();
+
+        vector<int>lps(size,0);
+
+        int right = 0 , centre = 0;
+
+        for(int i = 1 ; i < size-1; i++){
+            int mirror = 2 * centre - i;
+
+            if(right > i){
+                lps[i] = max(right - i,lps[mirror]);
+            }
+
+            while(ptr[i + lps[i] + 1] == ptr[i - lps[i] - 1]){
+                lps[i]++;
+            }
+
+            if(i + lps[i] > right){
+                centre = i;
+                right = i + lps[i];
             }
         }
 
-        return s.substr(ind,len);
+        int max_centre = 0 , max_len = 0;
+
+        for(int i =1; i < lps.size()-1 ; i++){
+            if(lps[i] > max_len){
+                max_len = lps[i];
+                max_centre = i;
+            }
+        }
+
+        return str.substr((max_centre  - max_len)/2  , max_len);
+    }
+
+    string longestPalindrome(string s) {
+        // int size = s.size();
+        // int ind = 0 , len = 0 ;
+
+        // for(int i = 0 ; i < size; i++){
+            
+        //     int len1 = solve(s,i,i);
+        //     int len2 = solve(s,i,i+1);
+
+        //     int max_len = max(len1,len2);
+
+        //     if(max_len > len){
+        //         len = max_len;
+        //         ind = i - (max_len -1)/2;
+        //     }
+        // }
+
+        // return s.substr(ind,len);
+
+        return manchers(s); 
     }
 };
