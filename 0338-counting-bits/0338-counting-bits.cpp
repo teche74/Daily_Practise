986383abebd1c48524a75cf8cb50f195b325841e
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 10:26:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (46.01%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>res(n+1,0);
+        vector<int>res(n+1,  0);
 
-        for(int i = 1; i <= n ; i++){
-            //  half the bit  && aloso check if it is odd;
-            res[i] = res[i/2];
+        for(int  i = 0 ; i <= n ; i++){
+            res[i] = res[i >> 1];
 
             if(i & 1){
                 res[i]++;
             }
         }
+
         return res;
     }
 };
