e1313e951a2a28792302631f4615172f2ccc77af
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 10:15:28 +0530

    Time: 46 ms (35.14%), Space: 13.2 MB (92.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int dp[13][10004];
-    int solve(int index, vector<int> & coins , int value){
-        if(value == 0 ) return 0;
-
-        if(index >= coins.size()){
+    int solve(int index , vector<int> & coins , int amount){ 
+        if(amount == 0 ) return 0;
+        
+        if(index >= coins.size() || amount < 0){
             return 1e9;
         }
 
-        if(dp[index][value] != -1) return dp[index][value];
+        if(dp[index][amount] != -1) return dp[index][amount];
 
-        int take = 1e9 , not_take = 1e9;
+        int take =  0 , not_take = 0;
 
-        if(coins[index] <= value){
-            take = 1 + solve(index,coins,value-coins[index]); 
-        }
-        not_take = solve(index+1,coins, value);
 
-        return dp[index][value] = min(take, not_take);
+        take = 1 + solve(index , coins,amount - coins[index]);
+    
+
+        not_take = solve(index + 1, coins , amount);
+
+
+        return dp[index][amount] = min(take , not_take);
     }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int res = solve(0 , coins, amount);
+        memset(dp , -1,sizeof(dp));
 
-        return res == 1e9 ? -1 : res;
+        int val = solve(0, coins , amount);
+
+        if(val == 1e9) return -1;
+        return val;
     }
 };
