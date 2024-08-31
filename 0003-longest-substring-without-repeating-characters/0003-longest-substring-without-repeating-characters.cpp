ddfabaf424f9a60f49d5c3913740c3832f0a352b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 08:16:58 +0530

    Time: 4 ms (90.57%), Space: 10.9 MB (82.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,56 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // approach 1 : set;
+        int freq[256];
+        memset(freq, -1, sizeof(freq));
 
-        // int low = 0 , high = 0 , size = s.size() , res = 0;
-        // unordered_set<char>st;
+        int low=  0 , high= 0 , size = s.size(), res = 0;
 
+        for(high = 0 ; high < size ; high++){
+            low = max(low,freq[s[high]]+1);
+            res = max(res , high - low+1 );
+            freq[s[high]] = high; 
+        } 
 
-        // while(high  < size){
-        //     if(st.find(s[high]) == st.end()){
-        //         st.insert(s[high++]);
-        //         res = max(res, high - low);
-        //     }
-        //     else{
-        //         st.erase(s[low++]);
-        //     }
-        // }
-        // return res;
-
-
-        // unordereded map  approach 2
-
-        // int low = 0 , high = 0 , size = s.size() , res = 0 ;
-        // unordered_map<char,int>map;
-
-        // while(high < size){
-        //     map[s[high]]++;
-
-        //     while(map[s[high]] > 1){
-        //         map[s[low]]--;
-        //         low++;
-        //     }
-
-        //     res = max(res, high - low+1);
-        //     high++;
-        // } 
-
-        // return res;
-
-
-        // quick access (last seen) 3rd apporach
-
-        int map[256];
-
-        memset(map,-1, sizeof(map));
-        int low = 0, size = s.size(), res=  0;
-
-        for(int high = 0 ;high < size ;high++){
-            low = max(low, map[s[high]]+1);
-            res= max(res, high - low+1);
-            map[s[high]] = high;
-        }
         return res;
     }
 };
