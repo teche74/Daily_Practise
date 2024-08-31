32e05ef2beec72c941c3cebae66c6c9bb2c1bf4b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 08:51:15 +0530

    Time: 103 ms (5.83%), Space: 106.9 MB (5.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int solve(int index , vector<int> & prices, int action , vector<vector<int>> & dp){
+    int dp[100000][3];
+
+    int solve(int index, vector<int> & prices ,  int action){
         if(index >= prices.size() || action == 0){
-            return 0;
+            return  0;
         }
 
         if(dp[index][action] != -1) return dp[index][action];
 
-        int act = 0 , leave = 0;
+        int buy =  0 , leave = 0;
+        
+        leave = solve(index+1,prices, action);
 
-        leave = solve(index+1,prices,action,dp);
         if(!(action & 1)){
-            act += -prices[index] + solve(index+1,prices,action-1,dp);
+            buy = -prices[index] + solve(index + 1, prices, action-1);
         }
 
         if(action & 1){
-            act += prices[index] + solve(index+1,prices,action-1,dp);
+            buy = prices[index] + solve(index + 1 , prices , action-1);
         }
 
-
-        return dp[index][action]  = max(act,leave);
+        return dp[index][action] = max(buy ,leave);
     }
+
     int maxProfit(vector<int>& prices) {
-        // vector<vector<int>>dp(prices.size()+1,vector<int>(3,-1));
-        // return solve(0,prices,2,dp);
-
-        int size = prices.size();
-        int min_price = INT_MAX,res = 0;
-
-        for(int i =0; i  <size; i++){
-            min_price = min(prices[i] , min_price);
-            res = max(res, prices[i] - min_price);
-        }
-
-        return res;
+        memset(dp , -1, sizeof(dp));
+        return solve( 0 ,prices, 2);    
     }
 };
