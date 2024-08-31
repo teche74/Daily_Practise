a538f7002eefe45b9ee69698d2882f7038abf8c2
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 09:38:33 +0530

    Time: 57 ms (59.76%), Space: 8.4 MB (89.1%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,49 +1,26 @@
-// class Solution {
-// public:
-//     int solve(int index, int val){
-//         if(val == 0){
-//             return 0;
-//         }
-        
-//         if(index * index > val) return 1e8;
-
-//         int take = 0 , not_take = 0;
-
-//         if(index * index <= val){
-//             take = 1 + solve(index , val - (index * index));
-//         }
-
-//         not_take = solve(index + 1,val);
-
-//         return min(take, not_take);
-
-//     }
-//     int numSquares(int n) {
-//         return solve(1,n);
-//     }
-// };
-
 class Solution {
 public:
-    int solve(int val, vector<int>&dp){
-        int count = INT_MAX;
-        int res = 0;
-        if(val == 0){
+    int dp[10004];
+
+    int solve(int val){
+        if(val <= 0){
             return 0;
         }
 
         if(dp[val] != -1) return dp[val];
 
-        for(int i =1; i <= sqrt(val); i++){
-            res  = 1 + solve( val - (i*i) ,dp );
-            count = min(count ,res);
+        int count =INT_MAX;
+
+        for(int i = 1 ; i *  i <= val ; i++){
+            int t =1 + solve(val - (i * i));
+            count  = min(count , t);
         }
-
         return dp[val] = count;
-
     }
+   
     int numSquares(int n) {
-        vector<int>dp(n + 1,-1); 
-        return solve(n, dp);
+        memset(dp , -1,sizeof(dp));
+
+        return solve(n);
     }
 };
