4e8b7787a71552aea759faab9053ba2ef39c83d4
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2024-08-31 08:56:43 +0530

    Time: 7 ms (30.57%), Space: 16.6 MB (76.06%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int dp[1005];
+    int dp[1001];
 
-    int solve(int index, vector<int>  & cost){
-        if(index >= cost.size()) return 0;
+    int solve(int index, vector<int> & cost){
+        if(index >= cost.size()){
+            return 0;
+        }
 
-        if(dp[index] != -1) return  dp[index];
+        if(dp[index] != -1) return dp[index];
 
-        int res = 1e9;
-
-        res = cost[index] + min(solve(index+1, cost) , solve(index+2,cost));
-
-        return dp[index] = res;
+        return dp[index] = cost[index] + min(solve(index+1,cost) , solve(index+2,cost));
     }
 
-
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(dp, -1, sizeof(dp));
-        return min(solve(0, cost) , solve(1, cost));
+        memset(dp ,-1 , sizeof(dp));
+
+        return min(solve( 0 , cost) ,solve(1 , cost));     
     }
 };
