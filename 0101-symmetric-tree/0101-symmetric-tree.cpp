767a3cef156409f5b912a1a7af0b55533c526889
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-06-25 22:10:58 +0530

    Time: 0 ms (100%), Space: 18.6 MB (29.55%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,14 +11,13 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode  * p , TreeNode *q ){
+    bool isSame(TreeNode * p ,  TreeNode * q){
         if(!p && !q) return true;
-        if(!p || !q) return false;
-        if(p->val != q->val) return false;
+        if(!p || !q || p->val != q->val) return false;
 
-        return solve(p->left,q->right) && solve(p->right,q->left);
+        return isSame(p->left,q->right) && isSame(p->right , q->left);
     }
     bool isSymmetric(TreeNode* root) {
-        return solve(root->left,root->right);
+        return isSame(root->left , root->right);
     }
 };
