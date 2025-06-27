eb0d82b91bbbb8d09d9845c5a85591412dcb8861
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-06-27 10:14:11 +0530

    Time: 0 ms (100%), Space: 12.3 MB (85.87%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -1,20 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void solve(TreeNode* root, int& prev, int& val) {
-        if (root) {
-            solve(root->left, prev, val);
-            if (prev != -1) {
-                val = min(val, root->val - prev);
+    void solve(TreeNode * root, TreeNode * & prev , int  &res){
+        if(root){
+            solve(root->left, prev, res);
+            if(prev){
+                res = min(res,  root->val - prev->val);
             }
-            prev = root->val;
-            solve(root->right, prev, val);
+            prev = root;
+            solve(root->right,prev,res);
         }
     }
-
     int minDiffInBST(TreeNode* root) {
-        int prev = -1;
         int res = INT_MAX;
+        TreeNode * prev = nullptr;
         solve(root, prev, res);
         return res;
     }
