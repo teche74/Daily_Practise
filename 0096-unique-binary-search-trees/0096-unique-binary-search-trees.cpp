11ea878fd269329d929e2612ce8cc41de81d1516
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-06-30 18:41:21 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.4%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
-    int numTrees(int n) {
-        int dp[n+1];
 
-        memset(dp ,0 ,sizeof(dp));
+    int numTrees(int n) {
+
+        function<vector<TreeNode *>(int,int)> solve = [&](int low, int high)->vector<TreeNode*>{
+            if(low > high) return {nullptr};
+            vector<TreeNode*>res;
+
+            for(int  i =low ; i <= high ; i++){
+                vector<TreeNode*>left = solve(low , i-1);
+                vector<TreeNode*> right = solve(i+1, high);
+                
+
+                for(auto l : left){
+                    for(auto r : right){
+                        TreeNode * root = new TreeNode(i);
+                        root->left = l;
+                        root->right = r;
+                        res.push_back(root);
+                    }
+                }
+            }
+
+            return res;
+        };
+
+        // return solve(1,n).size();
+
+        int dp[20];
+        memset(dp , 0 ,sizeof(dp));
 
         dp[0] = 1;
         dp[1] = 1;
 
-        for(int nodes = 2; nodes <= n ; nodes++){
-            for(int left = 0 ;left < nodes ; left++){
-                int right = nodes - left-1;
-
-                dp[nodes] += dp[left] * dp[right]; 
+        for(int total_nodes = 2; total_nodes <= n; total_nodes++){
+            for(int left = 0 ; left < total_nodes ; left++){
+                int right = total_nodes - left - 1;
+                dp[total_nodes]+= (dp[left] * dp[right]);
             }
         }
 
