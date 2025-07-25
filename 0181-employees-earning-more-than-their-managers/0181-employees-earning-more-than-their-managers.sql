f727e149d704b4b845d4472c61c10a1827fd820a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-07-25 12:30:30 +0530

    Time: 1294 ms (5%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,1 +1,4 @@
-select e2.name as Employee from Employee e1 left join employee e2 on e1.id = e2.managerId where e1.salary < e2.salary;
+-- select e2.name as Employee from Employee e1 left join employee e2 on e1.id = e2.managerId where e1.salary < e2.salary;
+
+
+select name as Employee from employee x where salary > (select salary from Employee where id = x.managerId ) and managerId is not null;
