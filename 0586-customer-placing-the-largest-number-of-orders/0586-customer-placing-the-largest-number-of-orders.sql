0f997a8e89e53808da680dad5fe8adc324870024
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-07-26 11:09:09 +0530

    Time: 726 ms (6.94%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,1 +1,5 @@
-select customer_number from Orders group by customer_number order by count(*) desc limit 1; 
+with modify_table as (
+    select customer_number , count(*) as total_buy from Orders group by customer_number
+)
+
+select customer_number from modify_table where total_buy = (select max(total_buy) from modify_table)
