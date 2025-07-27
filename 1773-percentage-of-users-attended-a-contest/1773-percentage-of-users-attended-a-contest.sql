1174b4d7405a7f4094d1cdb24187777b0b498d1a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-07-27 19:08:05 +0530

    Time: 965 ms (55.3%), Space: 0B (100%) - LeetHub

---
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,1 @@
+select r.contest_id, round(count(r.user_id)*100/(select count(user_id) from users),2) as percentage from Users u join Register r on u.user_id = r.user_id group by r.contest_id order by percentage desc , contest_id asc;
