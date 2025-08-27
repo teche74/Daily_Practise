0e5f27b76a4cc573d6c6c5af9352d5a71ce152bd
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-08-27 23:50:58 +0530

    Time: 0 ms (100%), Space: 18.9 MB (11.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,33 @@
+#define loop(i, start, end , step) for(int i = start ; i < end ; i+=step)
+#define pb push_back
+#define cond(value) if(value == 0)
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size()  , cols = matrix[0].size();
+        int rows = matrix.size() , cols = matrix[0].size();
 
-        vector<vector<int>>targets;
+        vector<pair<int,int>>indices;
 
-        for(int i = 0 ; i <rows ; i++){
-            for(int j = 0 ; j < cols ; j++){
-                if(matrix[i][j] == 0){
-                    targets.emplace_back(vector<int>{i,j});
+        loop(i, 0,rows, 1){
+            loop(j, 0, cols,1){
+                cond(matrix[i][j]){
+                    indices.pb({i,j});
                 }
             }
         }
 
-        if(targets.empty()) return ;
-
-        for(auto t : targets){
-            for(int i =  0; i < rows;  i++){
-                matrix[i][t[1]] = 0;
+        function<void(int, int)>make_zero = [&](int row, int col)->void{
+            for(int i = 0; i < rows ; i++){
+                matrix[i][col] = 0;
             }
-
-            for(int i = 0 ; i  <cols ; i++){
-                matrix[t[0]][i] = 0;
+            for(int j = 0 ; j < cols ; j++){
+                matrix[row][j] = 0;
             }
+        };
+
+        for(auto index_pair : indices){
+            make_zero(index_pair.first, index_pair.second);
         }
     }
 };
