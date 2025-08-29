58573c8e005bf60562f47dd5204927f43d57a442
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-08-29 20:00:38 +0530

    Time: 15 ms (10.86%), Space: 28 MB (5.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,13 +2,28 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         // sort and find
-        sort(nums.begin() , nums.end());
-        int size=  nums.size();
+        // sort(nums.begin() , nums.end());
+        // int size=  nums.size();
 
-        for(int i = 0; i < size ; i ++){
-            if(nums[i] != i) return i;
+        // for(int i = 0; i < size ; i ++){
+        //     if(nums[i] != i) return i;
+        // }
+
+        // return nums.size();
+
+
+        // using visit 
+
+        unordered_map<int,int>map;
+        int size = nums.size();
+
+        for(int val : nums){
+            map[val]++;
+        } 
+
+        for(int i = 0 ; i < size ; i++){
+            if(map.find(i) == map.end()) return i;
         }
-
-        return nums.size();
+        return size;
     }
 };
