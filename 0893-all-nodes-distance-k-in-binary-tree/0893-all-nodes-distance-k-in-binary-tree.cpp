8c5dc2bf517b8515f57042a4c8363e527e56f3b3
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-09-01 12:00:16 +0530

    Time: 7 ms (28.52%), Space: 15.9 MB (18.21%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,66 +9,62 @@
  */
 class Solution {
 public:
-    void make_parent_connection(TreeNode * root,unordered_map<TreeNode *, TreeNode *>& map ){
-        queue<TreeNode *> q;
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        if(!root) return {};
 
+        unordered_map<int,vector<int>>graph;
+
+        queue<TreeNode*>q;
         q.push(root);
+        vector<int>res;
 
-        while(!q.empty()){
+        while(q.size()){
             int size = q.size();
-            for(int i = 0 ;i < size; i++){
-                TreeNode * parent = q.front();
-                q.pop();
+            while(size--){
+                TreeNode* front = q.front();q.pop();
 
-                if(parent->left){
-                    q.push(parent->left);
-                    map[parent->left] = parent;
+                if(front->left){
+                    q.push(front->left);
+                    graph[front->val].push_back(front->left->val);
+                    graph[front->left->val].push_back(front->val);
                 }
-                
-                if(parent->right){
-                    q.push(parent->right);
-                    map[parent->right] = parent;
+
+                if(front->right){
+                    q.push(front->right);
+                    graph[front->val].push_back(front->right->val);
+                    graph[front->right->val].push_back(front->val);
                 }
             }
         }
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(!root) return {};
-        unordered_map<TreeNode *, TreeNode *> parent;
 
-        make_parent_connection(root,parent);
+        unordered_set<int>vis;
+        int level = 0;
+        
+        queue<int>q2;
+        q2.push(target->val);
+        vis.insert(target->val);
 
-        //  up left right call marta rhe
+        while(q2.size()){
+            int size =q2.size();
 
-        queue<pair<TreeNode *,int>> q;
-        map<TreeNode * ,bool>vis;
-
-        vector<int>res;
-
-        q.push({target,0});
-
-        while(!q.empty()){
-            TreeNode * curr = q.front().first;
-            int dist = q.front().second;
-            vis[curr] = true;
-            q.pop();
-
-            if(dist > k){
-                return res;
+            if (level == k) {
+                while (!q2.empty()) {
+                    res.push_back(q2.front());
+                    q2.pop();
+                }
+                break;
             }
-            if(dist <= k){
-                if(dist == k)res.emplace_back(curr->val);
-                if(parent[curr]  && !vis[parent[curr]]){
-                    q.push({parent[curr],dist+1});
-                }
-                if(curr->left && !vis[curr->left]){
-                    q.push({curr->left,dist+1});
-                }
 
-                if(curr->right && !vis[curr->right]){
-                    q.push({curr->right,dist+1});
+            while(size--){
+                int node = q2.front(); q2.pop();
+                for (int nei : graph[node]) {
+                    if (!vis.count(nei)) {
+                        vis.insert(nei);
+                        q2.push(nei);
+                    }
                 }
             }
+            level++;
         }
 
         return res;
