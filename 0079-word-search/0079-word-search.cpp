c1e0abeda7682f324537810a34151bf262c8d7fa
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-09-02 01:08:12 +0530

    Time: 678 ms (40.22%), Space: 12.5 MB (8.19%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,34 @@
+#define  loop(integer , start , stop) for(int integer = start ; integer < stop; integer++)
+
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int rows = board.size()  , cols = board[0].size();
+        int rows = board.size(), cols = board[0].size();
+        int index = 0;
 
-        vector<vector<int>>dirs = {{0,1},  {1,0} , {-1,0} , {0,-1}};
 
-        function<bool(int,int,int)> solve = [&](int index, int row, int col){
-            if(index >= word.size()) return true;
-            if(row >= rows || col  >= cols || row < 0 || col  < 0 || board[row][col] == '.' || board[row][col] != word[index]) return false;
-
-            char ch  = board[row][col];
+        function<bool(int,int)>dfs= [&](int row, int col){
+            if(index == word.size()) return true;
+            if(row >=rows || col >= cols || row < 0 || col < 0 || board[row][col] != word[index]) return false;
+            char ch = board[row][col];
             board[row][col] = '.';
             index++;
 
-            if(solve(index , row+1,col) || solve(index , row,col+1) || solve(index , row-1, col) || solve(index, row, col-1)) return true;
+            bool cond = dfs(row+1 , col) || dfs(row-1, col) || dfs(row, col+1) || dfs(row, col-1);
 
+            index--;
             board[row][col] = ch;
-            return false;
+            return cond;
         };
 
-        for(int i = 0; i  < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(board[i][j] == word[0] && solve(0,i,j)){
-                    return true;
+        loop(i,0,rows){
+            loop(j,0,cols){
+                if(board[i][j] == word[0]){
+                    if(dfs(i,j))
+                        return true;
                 }
             }
         }
-
         return false;
     }
 };
