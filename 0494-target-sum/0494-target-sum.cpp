8e7a21f1947f79f90e001847d7596743b18327b6
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-09-02 14:51:46 +0530

    Time: 15 ms (53.01%), Space: 12.6 MB (65.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int solve(int index, vector<int> & nums, int sum, unordered_map<string, int> & memo ){
-        if(index >= nums.size()){
-            return sum == 0;
-        }
-
-        string key = to_string(index) + "_" + to_string(sum);
-
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-
-        int add =  solve(index+1 , nums ,sum - nums[index],memo);
-        int sub =  solve(index+1,nums,sum + nums[index], memo);
-
-        return memo[key] = add + sub;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> memo;
-        return solve(0, nums, target, memo);
+        int size = nums.size();
+        int dp[21][2001];
+        memset(dp , -1 , sizeof(dp));
+
+        function<int(int , int)> call = [&](int index, int sum)->int{
+            if(index >= size){
+                return target == sum;
+            }
+
+            if(dp[index][sum+1000] != -1) return dp[index][sum+1000];
+
+            return dp[index][sum+1000] = call(index+1 , sum - nums[index]) + call(index+1, sum + nums[index]);
+        };
+
+
+        return call(0,0);
     }
 };
