b64eec72d5ee2acc8e52dfa392609f2cfe2f4125
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-10-26 11:16:44 +0530

    Time: 4 ms (35.05%), Space: 14.3 MB (70.5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string>res;
+        vector<string>res;  
         string temp;
 
-        function<void(int,int)> call = [&](int open , int close){
+        function<void(int,int)> check = [&](int open ,  int close){
             if(!open && !close){
                 res.push_back(temp);
                 return;
@@ -12,17 +12,20 @@ public:
 
             if(open){
                 temp.push_back('(');
-                call(open-1 , close);
+                check(open-1 , close);
                 temp.pop_back();
             }
-            if(close && close > open){
-                temp.push_back(')');
-                call(open, close-1);
-                temp.pop_back();
+            if(close){
+                if(open < close){
+                    temp.push_back(')');
+                    check(open , close-1);
+                    temp.pop_back();
+                }
             }
         };
 
-        call(n,n);
+        check(n,n);
+
         return res;
     }
 };
