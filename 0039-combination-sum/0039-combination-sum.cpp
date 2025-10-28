707b48ddca3af34218a24567c9896e093bee493b
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-10-28 08:33:58 +0530

    Time: 4 ms (40.84%), Space: 19.6 MB (21.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
+        set<vector<int>>res;
         vector<int>temp;
         int size = candidates.size();
 
-        function<void(int , int)> call = [&](int index , int target){
+        function<void(int , int)> solve =[&](int index , int target){
             if(index >= size){
                 if(!target){
-                    res.push_back(temp);
+                    res.insert(temp);
                 }
                 return;
             }
-
-            if(target - candidates[index] >= 0){
+            
+            if(candidates[index] <= target){
                 temp.push_back(candidates[index]);
-                call(index , target - candidates[index]);
+                
+                solve(index , target - candidates[index]);
+                
                 temp.pop_back();
             }
-
-            call(index+1 , target);
+            
+            solve(index+1, target);
+            
         };
 
-
-        call(0 , target);
-        return res;
+        solve(0 , target);
+        return {res.begin() , res.end()};
     }
 };
