adb785d98e1823e6fc734da8040323c6b6495e8d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-11-02 12:11:59 +0530

    Time: 198 ms (27.98%), Space: 209.1 MB (25.23%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,59 +1,51 @@
 class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        unordered_map<int, vector<int>> row_map, col_map;
-        set<pair<int,int>> occupied;
+        
+        vector<vector<int>>board(m , vector<int>(n, -1));
 
-        for (auto &g : guards) {
-            row_map[g[0]].push_back(g[1]);
-            col_map[g[1]].push_back(g[0]);
-            occupied.insert({g[0], g[1]});
-        }
-        for (auto &w : walls) {
-            row_map[w[0]].push_back(w[1]);
-            col_map[w[1]].push_back(w[0]);
-            occupied.insert({w[0], w[1]});
+        for(auto t : walls){
+            board[t[0]][t[1]] = 2;
         }
 
-        for (auto &it : row_map) sort(it.second.begin(), it.second.end());
-        for (auto &it : col_map) sort(it.second.begin(), it.second.end());
+        int total_cells = (m * n) - walls.size();  
 
-        set<pair<int,int>> guarded;
-
-        for (auto &g : guards) {
-            int r = g[0], c = g[1];
-
-            auto &row = row_map[r];
-            auto it = lower_bound(row.begin(), row.end(), c);
-            int left = (it == row.begin()) ? -1 : *(prev(it));
-            for (int j = c-1; j > left; j--) {
-                if (occupied.count({r,j})) break;
-                guarded.insert({r,j});
-            }
-
-            int right = (next(it) == row.end()) ? n : *(next(it));
-            for (int j = c+1; j < right; j++) {
-                if (occupied.count({r,j})) break;
-                guarded.insert({r,j});
-            }
-
-            auto &col = col_map[c];
-            auto it2 = lower_bound(col.begin(), col.end(), r);
-            int up = (it2 == col.begin()) ? -1 : *(prev(it2));
-            for (int i = r-1; i > up; i--) {
-                if (occupied.count({i,c})) break;
-                guarded.insert({i,c});
-            }
-
-            int down = (next(it2) == col.end()) ? m : *(next(it2));
-            for (int i = r+1; i < down; i++) {
-                if (occupied.count({i,c})) break;
-                guarded.insert({i,c});
+        for(auto t : guards){
+            if (board[t[0]][t[1]] != 1){
+                board[t[0]][t[1]] = 1;
+                total_cells--;
             }
         }
 
-        long long total_empty = (long long)m * n - occupied.size();
-        long long unguarded = total_empty - guarded.size();
-        return (int)unguarded;
+        for(auto t : guards){
+            int row = t[0] , col = t[1];
+            
+
+            for(int j = row-1; j >= 0; j--){
+                if(board[j][col] == 2 || board[j][col] == 1) break;
+                total_cells -= (board[j][col] == -1);
+                board[j][col] = 3;
+            }
+
+            for(int j = row+1; j < m; j++){
+                if(board[j][col] == 2 || board[j][col] == 1) break;
+                total_cells-=(board[j][col] == -1);
+                board[j][col] = 3;
+            }
+
+            for(int j = col-1 ; j >= 0; j--){
+                if(board[row][j] == 2 ||  board[row][j] == 1) break;
+                total_cells-=(board[row][j] == -1);
+                board[row][j] = 3;
+            }
+
+            for(int j = col + 1;j < n; j++){
+                if(board[row][j] == 2 ||  board[row][j] == 1)break;
+                total_cells-=(board[row][j] == -1);
+                board[row][j] = 3;
+            }
+        }
+
+        return total_cells;
     }
 };
